56d91b9bc714610eda83d2b7daa50d80751bcb48
Nagi <nagi@ndscm.com>
2025-02-09 14:06:03 +0800

    seed: buildinfo: always generate all variables
    Change-uuid: 0308b8af-8a31-5257-aafe-761173f1e89a

--- seed/devprod/buildinfo/generate-workspace-status.sh
+++ seed/devprod/buildinfo/generate-workspace-status.sh
@@ -3,32 +3,32 @@ set -euo pipefail
 
 # Build Info
 
-dirty=$(git status --porcelain | wc -l)
-branch=$(git branch --show-current)
-tag=$(git describe --tags)
+if [[ $(git rev-parse --is-inside-work-tree) ]]; then
+  dirty=$(git status --porcelain | wc -l)
+  branch=$(git branch --show-current)
+  tag=$(git describe --tags)
 
-brief=${tag}
-if [ "$branch" != "main" ]; then
-  brief="${brief} on ${branch}"
-fi
-if [ "$dirty" != "0" ]; then
-  brief="${brief}~${dirty}"
-fi
+  brief=${tag}
+  if [ "$branch" != "main" ]; then
+    brief="${brief} on ${branch}"
+  fi
+  if [ "$dirty" != "0" ]; then
+    brief="${brief}~${dirty}"
+  fi
 
-echo STABLE_BUILD_TAG ${tag}
-echo STABLE_BUILD_BRANCH ${branch}
-echo STABLE_BUILD_DIRTY ${dirty}
-echo STABLE_BUILD_BRIEF ${brief}
-echo STABLE_GIT_COMMIT $(git rev-parse HEAD)
-
-
-
-
-
-
-
-
+  commit=$(git rev-parse HEAD)
 fi
 
+echo STABLE_BUILD_TAG ${tag:-"unknown"}
+echo STABLE_BUILD_BRANCH ${branch:-"unknown"}
+echo STABLE_BUILD_DIRTY ${dirty:-"unknown"}
+echo STABLE_BUILD_BRIEF ${brief:-"unknown"}
+echo STABLE_GIT_COMMIT ${commit:-"unknown"}
 
-fi
+
+
+
+
+
+
+
