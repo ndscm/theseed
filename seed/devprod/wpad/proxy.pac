5b4b9e32ee941e4075549f3f8b1d82929d2cb35a
Nagi <nagi@ndscm.com>
2025-02-15 07:54:52 +0800

    seed: wpad: initialize wpad server
    Change-uuid: c6f0c6df-395e-52fc-a44b-b7aa0db535fb

---
+++ seed/devprod/wpad/proxy.pac
@@ -0,0 +1,48 @@
+var proxy = "DIRECT"
+
+var whitelist = {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  go: 1,
+}
+
+function FindProxyForURL(url, host) {
+  var dot
+  do {
+    if (whitelist.hasOwnProperty(host)) {
+      return "DIRECT"
+    }
+    dot = host.indexOf(".")
+    host = host.slice(dot + 1)
+  } while (dot >= 0)
+  return proxy
+}
---
+++ seed/devprod/wpad/wpad.js
@@ -0,0 +1,34 @@
+var fs = require("node:fs")
+var http = require("node:http")
+var process = require("node:process")
+
+var server = http.createServer(function (request, response) {
+  if (request.url != "/wpad.dat" && request.url != "/proxy.pac") {
+    console.error("Invalid url: ", request.url)
+  }
+  console.info("Request")
+  fs.readFile("./proxy.pac", { encoding: "utf-8" }, function (error, content) {
+    if (error) {
+      if (error.code == "ENOENT") {
+        response.writeHead(404)
+        response.end("404", "utf-8")
+      } else {
+        response.writeHead(500)
+        response.end("Error: " + error.code, "utf-8")
+      }
+    } else {
+      content = content.replace(
+        /^var[ ]proxy[ ][=].*/,
+        'var proxy = "' + process.env["WPAD_TARGET"] + '";'
+      )
+      response.writeHead(200, {
+        "Content-Type": "application/x-ns-proxy-autoconfig",
+      })
+      response.end(content, "utf-8")
+    }
+    console.info("Response")
+  })
+})
+
+server.listen(9723, "0.0.0.0")
+console.info("Wpad server started")
