e9b0aebbbeddb3d97f16e4a26bffe111b637be58
Nagi <nagi@ndscm.com>
2025-03-24 10:56:20 +0800

    seed: ndscm: initialize entrypoint for shell to source
    Change-uuid: c16538b0-7067-5c80-9346-30a25a8b3720

---
+++ seed/devprod/ndscm/envsetup.sh
@@ -0,0 +1,65 @@
+if [[ -z "${ND_MONOREPO_HOME}" &&
+  -d "${ND_MONOREPO_HOME}/" &&
+  -z "${ND_MONOREPO_GIT_DIR}" &&
+  -d "${ND_MONOREPO_GIT_DIR}/" ]]; then
+  export ND_MONOREPO_HOME=$(realpath "${ND_MONOREPO_HOME}/")
+  export ND_MONOREPO_GIT_DIR=$(realpath "${ND_MONOREPO_GIT_DIR}/")
+fi
+
+function nd-quick-verify-monorepo {
+  if [[ -z "${ND_MONOREPO_HOME}" ]]; then
+    echo -e "\e[31mND_MONOREPO_HOME is not set\e[0m"
+    return 1
+  fi
+  if [[ -z "${ND_MONOREPO_GIT_DIR}" ]]; then
+    echo -e "\e[31mND_MONOREPO_GIT_DIR is not set\e[0m"
+    return 1
+  fi
+  if [[ ! -d "${ND_MONOREPO_HOME}" ]]; then
+    echo -e "\e[31mND_MONOREPO_HOME (${ND_MONOREPO_HOME}) is not a valid folder\e[0m"
+    return 1
+  fi
+  if [[ ! -d "${ND_MONOREPO_GIT_DIR}" ]]; then
+    echo -e "\e[31mND_MONOREPO_GIT_DIR (${ND_MONOREPO_GIT_DIR}) is not a valid folder\e[0m"
+    return 1
+  fi
+
+}
+
+function nd-dev {
+  if ! nd-quick-verify-monorepo; then
+    return $?
+  fi
+  if [[ "$#" -eq 0 ]]; then
+    cd "${ND_MONOREPO_HOME}/dev"
+  elif [[ "$#" -eq 1 ]]; then
+    worktree_name="dev-${1}"
+    if [[ -d "${ND_MONOREPO_HOME}/${worktree_name}" ]]; then
+      cd "${ND_MONOREPO_HOME}/${worktree_name}"
+    else
+      git --git-dir "${ND_MONOREPO_GIT_DIR}" worktree add -B "${worktree_name}" "${ND_MONOREPO_HOME}/${worktree_name}" origin/main
+      cd "${ND_MONOREPO_HOME}/${worktree_name}"
+    fi
+  else
+    echo -e "\e[31mToo many params for nd-dev.\e[0m"
+    return 1
+  fi
+}
+
+function nd {
+  if [[ "$#" -eq 0 ]]; then
+    echo "nd is not distributed source code manager"
+  elif [[ "$#" -ge 1 ]]; then
+    case "${1}" in
+    "dev")
+      nd-dev ${@:2}
+      ;;
+    *)
+      echo -e "\e[31mUnknown command ${1}\e[0m"
+      ;;
+    esac
+  else
+    echo -e "\e[31mUnknown params ${@}\e[0m"
+    return 1
+  fi
+}
