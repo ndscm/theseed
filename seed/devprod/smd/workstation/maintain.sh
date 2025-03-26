8eef61fa237319b01c026af4587915e0add1a351
Nagi <nagi@ndscm.com>
2025-03-26 19:45:14 +0800

    seed: smd: configure default mergetool as p4merge
    Change-uuid: 2179e538-4787-5071-ac5a-c10b7010d3a4

--- seed/devprod/smd/workstation/maintain.sh
+++ seed/devprod/smd/workstation/maintain.sh
@@ -207,6 +207,25 @@ fi
 
 echo -e "\e[32mCheck snap tools done.\e[0m"
 
+# ## Git
+
+git config --global core.autocrlf input
+git config --global diff.tool p4merge
+if ${wsl}; then
+  git config --global difftool.p4merge.cmd 'p4merge.exe "$LOCAL" "$REMOTE"'
+else
+  git config --global difftool.p4merge.cmd 'p4merge "$LOCAL" "$REMOTE"'
+fi
+git config --global difftool.p4merge.trustExitCode "true"
+git config --global merge.tool p4merge
+if ${wsl}; then
+  git config --global mergetool.p4merge.cmd 'p4merge.exe "$BASE" "$LOCAL" "$REMOTE" "$MERGED"'
+else
+  git config --global mergetool.p4merge.cmd 'p4merge "$BASE" "$LOCAL" "$REMOTE" "$MERGED"'
+fi
+git config --global mergetool.p4merge.trustExitCode "true"
+git config --global mergetool.keepBackup "false"
+
 # ## Golang
 
 echo -e "\e[34mChecking golang tools...\e[0m"
