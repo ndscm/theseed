cf8e489b5999b9928e8061146aa9125e32b1f1de
Nagi <nagi@ndscm.com>
2025-03-27 11:28:23 +0800

    seed: ndscm: parse flags in arbitrary option orders
    Change-uuid: 847181d1-d72e-5480-aa47-47e4263691e1

--- seed/devprod/ndscm/cli/ndscm.go
+++ seed/devprod/ndscm/cli/ndscm.go
@@ -2,40 +2,63 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/ndscm/theseed/seed/devprod/ndscm/common"
 )
 
+func parseFlags() ([]string, error) {
+	remainArgs := os.Args
+	finalArgs := []string{}
+	for len(remainArgs) > 0 {
+		finalArgs = append(finalArgs, remainArgs[0])
+		err := flag.CommandLine.Parse(remainArgs[1:])
+		if err != nil {
+			return nil, common.WrapTrace(err)
+		}
+		remainArgs = flag.CommandLine.Args()
+	}
+	return finalArgs[1:], nil
+}
+
 func main() {
-	flag.Parse()
+	args, err := parseFlags()
+	if err != nil {
+		log.Fatalf("\x1b[31mERROR: parse flags failed: %v\x1b[0m", err)
+	}
 	ndConfig, err := common.LoadConfig()
 	if err != nil {
 		log.Fatalf("\x1b[31mERROR: load config failed: %v\x1b[0m", err)
 	}
-	switch flag.Arg(0) {
+	if len(args) < 1 {
+		fmt.Printf("ndscm is not-distributed source code manager\n")
+		return
+	}
+	switch args[0] {
 	case "cut":
-		err := NdCut(flag.Args(), ndConfig)
+		err := NdCut(args, ndConfig)
 		if err != nil {
 			log.Fatalf("\x1b[31mERROR: %v\x1b[0m", err)
 		}
 	case "dev":
-		err := NdDev(flag.Args(), ndConfig)
+		err := NdDev(args, ndConfig)
 		if err != nil {
 			log.Fatalf("\x1b[31mERROR: %v\x1b[0m", err)
 		}
 	case "shell":
-		err := NdShell(flag.Args(), ndConfig)
+		err := NdShell(args, ndConfig)
 		if err != nil {
 			log.Fatalf("\x1b[31mERROR: %v\x1b[0m", err)
 		}
 	case "sync":
-		err := NdSync(flag.Args(), ndConfig)
+		err := NdSync(args, ndConfig)
 		if err != nil {
 			log.Fatalf("\x1b[31mERROR: %v\x1b[0m", err)
 		}
 	default:
+		log.Fatalf("\x1b[31mERROR: Unknown command %v\x1b[0m", args[0])
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
