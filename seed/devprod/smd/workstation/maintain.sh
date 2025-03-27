4f84af128aa4572b5667d252df331b79d058a5f8
Nagi <nagi@ndscm.com>
2025-03-27 11:26:41 +0800

    seed: smd: install go language server
    Change-uuid: 5832b775-0300-5243-8bbc-d5f880d33fb2

--- seed/devprod/smd/workstation/maintain.sh
+++ seed/devprod/smd/workstation/maintain.sh
@@ -291,7 +291,15 @@ if [[ "${oslike}" == "debian" || "${oslike}" == "darwin" ]]; then
 # # Golang
 export PATH="\$HOME/go/bin:\$PATH"
 EOF
-  go install github.com/bazelbuild/buildtools/buildifier@latest
+  go install -v github.com/bazelbuild/buildtools/buildifier@latest
+  go install -v github.com/cweill/gotests/gotests@latest
+  go install -v github.com/fatih/gomodifytags@latest
+  go install -v github.com/go-delve/delve/cmd/dlv@latest
+  go install -v github.com/haya14busa/goplay/cmd/goplay@latest
+  go install -v github.com/josharian/impl@latest
+  go install -v golang.org/x/tools/cmd/goimports@latest
+  go install -v golang.org/x/tools/gopls@latest
+  go install -v honnef.co/go/tools/cmd/staticcheck@latest
 fi
 
 printf "\e[32mCheck golang tools done.\e[0m\n"
