cb09be1c9f37b14d489d76747ad0a160bdee5f84
Nagi <nagi@ndscm.com>
2025-03-28 20:21:53 +0800

    seed: ndscm: add review command to push change
    Change-uuid: 6a334517-b510-5769-8e2e-1d58497b3f32

---
+++ seed/devprod/ndscm/cli/review.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/ndscm/theseed/seed/devprod/ndscm/common"
+)
+
+func NdReview(args []string, ndConfig *common.NdConfig) error {
+	if ndConfig.ShellEval {
+		return common.WrapTrace(fmt.Errorf("nd-review should not run with --shell-eval"))
+	}
+	if len(args) != 2 {
+		return common.WrapTrace(fmt.Errorf("nd-review usage: nd review <feature-name>"))
+	}
+	if ndConfig.Scm == "git" {
+		err := common.QuickVerifyGitMonorepo(ndConfig)
+		if err != nil {
+			return err
+		}
+		featureName := strings.TrimSpace(args[1])
+		worktreePath := filepath.Join(ndConfig.MonorepoHome, "review/"+featureName)
+		_, err = os.Stat(worktreePath)
+		if err != nil && !os.IsNotExist(err) {
+			return common.WrapTrace(err)
+		}
+		if err == nil {
+			log.Printf("Worktree %v already exists, removing...\n", worktreePath)
+			err = common.ShellRun(ndConfig.Dry, ndConfig.ShellEval, "git", "worktree", "remove", worktreePath)
+			if err != nil {
+				return err
+			}
+		}
+		_, err = common.ShellOutput(false, "git", "rev-parse", "--verify", "review/"+featureName)
+		if err == nil {
+			log.Printf("Branch review/%v already exists, removing...\n", featureName)
+			err = common.ShellRun(ndConfig.Dry, ndConfig.ShellEval, "git", "branch", "--delete", "--force", "review/"+featureName)
+			if err != nil {
+				return err
+			}
+		}
+		trackingBranchOutput, err := common.ShellOutput(false, "git", "rev-parse", "--abbrev-ref", "--symbolic-full-name", "change/"+featureName+"@{upstream}")
+		if err != nil {
+			return common.WrapTrace(fmt.Errorf("tracking upstream is missing for change/%v", featureName))
+		}
+		trackingBranch := strings.TrimSpace(string(trackingBranchOutput))
+		mergeBaseHashOutput, err := common.ShellOutput(false, "git", "merge-base", "origin/main", "change/"+featureName)
+		if err != nil {
+			return common.WrapTrace(fmt.Errorf("merge base is missing for change/%v", featureName))
+		}
+		mergeBaseHash := strings.TrimSpace(string(mergeBaseHashOutput))
+		err = common.ShellRun(ndConfig.Dry, ndConfig.ShellEval, "git", "branch", "review/"+featureName, mergeBaseHash)
+		if err != nil {
+			return err
+		}
+		err = common.ShellRun(ndConfig.Dry, ndConfig.ShellEval, "git", "branch", "--set-upstream-to=origin/main", "review/"+featureName)
+		if err != nil {
+			return err
+		}
+		err = common.ShellRun(ndConfig.Dry, ndConfig.ShellEval, "git", "worktree", "add", worktreePath, "review/"+featureName)
+		if err != nil {
+			return err
+		}
+		err = common.ShellRun(ndConfig.Dry, ndConfig.ShellEval, "git", "-C", worktreePath, "cherry-pick", trackingBranch+"..change/"+featureName)
+		if err != nil {
+			return err
+		}
+		err = common.ShellRun(ndConfig.Dry, ndConfig.ShellEval, "git", "push", "--force", "origin", "review/"+featureName+":"+ndConfig.UserHandle+"/"+featureName)
+		if err != nil {
+			return err
+		}
+		err = common.ShellRun(ndConfig.Dry, ndConfig.ShellEval, "git", "worktree", "remove", worktreePath)
+		if err != nil {
+			return err
+		}
+		err = common.ShellRun(ndConfig.Dry, ndConfig.ShellEval, "git", "branch", "--delete", "--force", "review/"+featureName)
+		if err != nil {
+			return err
+		}
+	} else {
+		return common.WrapTrace(fmt.Errorf("nd-review does not support %v", ndConfig.Scm))
+	}
+	return nil
+}
