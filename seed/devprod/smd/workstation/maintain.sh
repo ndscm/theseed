55af159fb9e1f9c115aa0e9e77f8aa81cb240a7f
Nagi <nagi@ndscm.com>
2025-04-18 17:45:11 +0800

    seed: smd: mount monodrive with davfs2
    Change-uuid: 115174f2-66cb-5c78-a02f-ebb6163099bf

--- seed/devprod/smd/workstation/maintain.sh
+++ seed/devprod/smd/workstation/maintain.sh
@@ -224,6 +224,7 @@ printf "\e[34mChecking system packages...\e[0m\n"
 if [[ "${oslike}" == "debian" ]]; then
   sudo apt install -y clang
   sudo apt install -y clang-format
+  sudo apt install -y davfs2
   sudo apt install -y default-jdk
   sudo apt install -y g++
   sudo apt install -y gcc
@@ -387,6 +388,49 @@ export ND_MONOREPO_HOME="\$SEED_MONOREPO_HOME"
 export ND_MONOREPO_GIT_DIR="\$SEED_MONOREPO_GIT_DIR"
 export ND_USER_HANDLE="\$ND_USER_HANDLE"
 EOF
+
+# # Monodrive
+
+printf "\e[34mChecking theseed monodrive...\e[0m\n"
+
+if [[ "${oslike}" == "debian" ]]; then
+  davfs2_uid=$(id -u davfs2)
+  davfs2_gid=$(id -g davfs2)
+  if [[ -z "${SEED_MONODRIVE_LOGIN+x}" ]]; then
+    read -p "Enter mono drive user login: " SEED_MONODRIVE_LOGIN
+    cat <<EOF >>${HOME}/.managed_profile
+# # Monodrive
+export SEED_MONODRIVE_LOGIN="${SEED_MONODRIVE_LOGIN}"
+EOF
+  fi
+  if ! sudo grep '^[/]mnt[/]theseed[/]drive[ ]' /etc/davfs2/secrets; then
+    read -p "Enter mono drive user password: " SEED_MONODRIVE_PASSWORD
+    printf "\n/mnt/theseed/drive ${SEED_MONODRIVE_LOGIN} ${SEED_MONODRIVE_PASSWORD}\n" | sudo tee -a /etc/davfs2/secrets
+  fi
+  sudo usermod -aG davfs2 ${USER}
+  sudo mkdir -p /mnt/theseed/drive
+  cat <<EOF | sudo tee /usr/lib/systemd/system/mnt-theseed-drive.mount
+[Unit]
+Description=Mount Theseed Monodrive
+After=network-online.target
+Wants=network-online.target
+
+[Mount]
+What=https://drive.ndscm.com/remote.php/dav/files/${SEED_MONODRIVE_LOGIN}/theseed
+Where=/mnt/theseed/drive
+Options=uid=${davfs2_uid},gid=${davfs2_gid},file_mode=0664,dir_mode=2775,grpid
+Type=davfs
+TimeoutSec=15
+
+[Install]
+WantedBy=multi-user.target
+EOF
+  sudo systemctl daemon-reload
+  sudo systemctl enable mnt-theseed-drive.mount
+  sudo systemctl start mnt-theseed-drive.mount
+fi
+
+printf "\e[34mCheck theseed monodrive done.\e[0m\n"
 
 # # Shortcuts
 
