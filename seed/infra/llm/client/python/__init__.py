1631f82d54cf99557b955e64c19da56b360aeb44
Nagi <nagi@ndscm.com>
2025-06-24 13:29:27 +0800

    seed: llm: use seed_configs to load configs
    Change-uuid: 5f678460-dd86-525d-bbfc-a49854a1a411

--- seed/infra/llm/client/python/__init__.py
+++ seed/infra/llm/client/python/__init__.py
@@ -1,15 +1,31 @@
-import os
+import pydantic
 
 import seed.infra.python.seed_typing as seed_typing
 import seed.infra.llm.client.python.llm_client as llm_client
+import seed.infra.python.seed_flag as seed_flag
+import seed.infra.python.seed_init as seed_init
 
-_client = llm_client.LlmClient(
-    server=os.environ.get("LLM_SERVER", "https://ark.cn-beijing.volces.com/api/v3"),
-    model=os.environ.get("LLM_MODEL", "deepseek-v3-250324"),
-    api_key_file_path=os.environ.get(
-        "LLM_API_KEY_FILE", "${ND_USER_SECRET_HOME}/volcengine/ARK_API_KEY"
-    ),
+llm_server = seed_flag.define(
+    "llm_server", str, "https://ark.cn-beijing.volces.com/api/v3"
 )
+llm_model = seed_flag.define("llm_model", str, "deepseek-v3-250324")
+llm_api_key_file = seed_flag.define(
+    "llm_api_key_file", str, "${ND_USER_SECRET_HOME}/volcengine/ARK_API_KEY"
+)
+
+_client: llm_client.LlmClient
+
+
+def init():
+    global _client
+    _client = llm_client.LlmClient(
+        server=llm_server.get(),
+        model=llm_model.get(),
+        api_key_file_path=llm_api_key_file.get(),
+    )
+
+
+seed_init.register_module_init(init)
 
 
 @seed_typing.unbind_callable_type(llm_client.LlmClient.request)
