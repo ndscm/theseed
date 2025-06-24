0ac457adc7ed9085a7596c8fa0cb39bfc46ac85f
Nagi <nagi@ndscm.com>
2025-06-24 20:39:37 +0800

    seed: yaya: migrate memories into newborn memories
    Change-uuid: 25f02b00-0615-5506-bf0f-d756746e3acf

--- seed/infra/mcp/client/python/mcp_client.py
+++ seed/infra/mcp/client/python/mcp_client.py
@@ -1,19 +1,24 @@
 import os
+import typing
 
 import fastmcp
+import fastmcp.client
 
 
 class McpClient:
     server: str
     service_name: str
-    _client: fastmcp.Client
+    _client: fastmcp.Client[fastmcp.client.StreamableHttpTransport]
 
     def __init__(self, server: str = "", service_name: str = ""):
         self.service_name = service_name
         self.server = server
         if not self.server:
             self.server = os.environ.get("MCP_SERVER", "http://127.0.0.1:6277")  # MCPS
-        self._client = fastmcp.Client(f"{server}/{service_name}/mcp/")
+        self._client = typing.cast(
+            fastmcp.Client[fastmcp.client.StreamableHttpTransport],
+            fastmcp.Client(f"{server}/{service_name}/mcp/"),
+        )
 
-    def client(self) -> fastmcp.Client:
+    def client(self) -> fastmcp.Client[fastmcp.client.StreamableHttpTransport]:
         return self._client
