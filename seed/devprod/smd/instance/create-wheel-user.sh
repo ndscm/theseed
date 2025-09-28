80be78092c28be5b5ec52d4df59712d84c92f280
Nagi <nagi@ndscm.com>
2025-09-28 19:49:58 +0800

    seed: smd: setup authorized keys only when user is already created
    Change-uuid: dd0cb903-22af-540e-9b39-f4f2fef30ba0

--- seed/devprod/smd/instance/create-wheel-user.sh
+++ seed/devprod/smd/instance/create-wheel-user.sh
@@ -10,6 +10,12 @@ user_authorized_key=${4-"$(cat ~/.ssh/id_ed25519.pub)"}
 
 ssh -i ${ND_USER_SECRET_HOME}/${cloud}/${ND_USER_HANDLE}.pem -t root@${server} "
 set -eux
+
+if [[ -d /home/${user_nd_user_handle} ]]; then
+  echo ${user_authorized_key} >>/home/${user_nd_user_handle}/.ssh/authorized_keys
+  exit 0
+fi
+
 useradd --create-home ${user_nd_user_handle}
 passwd ${user_nd_user_handle}
 usermod -a -G wheel ${user_nd_user_handle}
