2771babbbc48a1b9cb3e826f351d717f2cf97e86
Nagi <nagi@ndscm.com>
2026-03-11 05:05:30 +0800

    seed: golink: cleanup old golink
    Change-uuid: 214fcb4c-517b-519f-83d9-2e5e5dce9422

--- seed/devprod/golink/backup-golink.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Copy this script to /etc/cron.daily
-set -eux
-set -o pipefail
-
-mkdir -p /mnt/data/golink/backup
-datetime=$(date +%Y%m%d%H%M%S)
-cp /mnt/data/golink/golink.db /mnt/data/golink/backup/golink-${datetime}.db
