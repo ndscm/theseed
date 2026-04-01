69e1b470d68a3a4c7854979ab7b96fc9e089c437
Nagi <nagi@ndscm.com>
2026-04-01 19:27:57 +0800

    seed: golink: define error code in proto
    Change-uuid: 3a9b9638-75f4-5a5b-807d-8e61a5ac1aaa

--- seed/devprod/golink/proto/build.sh
+++ seed/devprod/golink/proto/build.sh
@@ -17,10 +17,19 @@ if [[ "${target}" == "" || "${target}" == *"go"* ]]; then
   cp -f \
     ./bazel-bin/seed/devprod/golink/proto/golinkpbconnect/golink.connect.go \
     ./seed/devprod/golink/proto/golinkpbconnect/golink.connect.go
+
+  bazel build --output_groups=+go_generated_srcs //seed/devprod/golink/proto:golink_error_go_proto
+  mkdir -p ./seed/devprod/golink/proto/golinkerrorpb
+  cp -f \
+    ./bazel-bin/seed/devprod/golink/proto/golink_error_go_proto_/github.com/ndscm/theseed/seed/devprod/golink/proto/golinkerrorpb/golink_error.pb.go \
+    ./seed/devprod/golink/proto/golinkerrorpb/golink_error.pb.go
 fi
 
 if [[ "${target}" == "" || "${target}" == *"ts"* ]]; then
   bazel build --output_groups=+types //seed/devprod/golink/proto:golink_ts_proto
   cp -f ./bazel-bin/seed/devprod/golink/proto/golink_pb.js ./seed/devprod/golink/proto/golink_pb.js
   cp -f ./bazel-bin/seed/devprod/golink/proto/golink_pb.d.ts ./seed/devprod/golink/proto/golink_pb.d.ts
+  bazel build --output_groups=+types //seed/devprod/golink/proto:golink_error_ts_proto
+  cp -f ./bazel-bin/seed/devprod/golink/proto/golink_error_pb.js ./seed/devprod/golink/proto/golink_error_pb.js
+  cp -f ./bazel-bin/seed/devprod/golink/proto/golink_error_pb.d.ts ./seed/devprod/golink/proto/golink_error_pb.d.ts
 fi
