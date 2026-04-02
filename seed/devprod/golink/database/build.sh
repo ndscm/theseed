e794250fb1629af2c8b4731a768f1d51f1a63c69
Nagi <nagi@ndscm.com>
2026-04-02 18:47:39 +0800

    seed: bootstrap: avoid rebuild ent during bootstrap
    Change-uuid: a401a933-443b-54ba-80fd-8bb37e3ac706

--- seed/devprod/golink/database/build.sh
+++ seed/devprod/golink/database/build.sh
@@ -3,7 +3,9 @@ set -eux
 set -o pipefail
 cd $(dirname "${BASH_SOURCE[0]}")/../../../..
 
-bazel build //seed/devprod/golink/database:ent_full_srcs
+if [[ -z "${SEED_MONOREPO_BOOTSTRAP:-}" ]]; then
+  bazel build //seed/devprod/golink/database:ent_full_srcs
+fi
 mkdir -p ./seed/devprod/golink/database/ent
 rsync --archive --delete \
   --include='*/' --include='*.go' --exclude='*' \
