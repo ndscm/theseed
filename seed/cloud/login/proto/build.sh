0faf0521235555d957570bcde26f97218aa09f50
Nagi <nagi@ndscm.com>
2026-04-02 18:41:10 +0800

    seed: bootstrap: avoid rebuild protos during bootstrap
    Change-uuid: 97c40d6f-966d-5672-b19f-bbfaab6f6a03

--- seed/cloud/login/proto/build.sh
+++ seed/cloud/login/proto/build.sh
@@ -6,13 +6,17 @@ cd $(dirname "${BASH_SOURCE[0]}")/../../../..
 IFS="+" target="${@:1}"
 
 if [[ "${target}" == "" || "${target}" == *"go"* ]]; then
-  bazel build --output_groups=+go_generated_srcs //seed/cloud/login/proto:login_go_proto
+  if [[ -z "${SEED_MONOREPO_BOOTSTRAP:-}" ]]; then
+    bazel build --output_groups=+go_generated_srcs //seed/cloud/login/proto:login_go_proto
+  fi
   mkdir -p ./seed/cloud/login/proto/loginpb
   cp -f \
     ./bazel-bin/seed/cloud/login/proto/login_go_proto_/github.com/ndscm/theseed/seed/cloud/login/proto/loginpb/login.pb.go \
     ./seed/cloud/login/proto/loginpb/login.pb.go
 
-  bazel build //seed/cloud/login/proto:login_go_connect_go
+  if [[ -z "${SEED_MONOREPO_BOOTSTRAP:-}" ]]; then
+    bazel build //seed/cloud/login/proto:login_go_connect_go
+  fi
   mkdir -p ./seed/cloud/login/proto/loginpbconnect
   cp -f \
     ./bazel-bin/seed/cloud/login/proto/loginpbconnect/login.connect.go \
@@ -20,7 +24,9 @@ if [[ "${target}" == "" || "${target}" == *"go"* ]]; then
 fi
 
 if [[ "${target}" == "" || "${target}" == *"ts"* ]]; then
-  bazel build --output_groups=+types //seed/cloud/login/proto:login_ts_proto
+  if [[ -z "${SEED_MONOREPO_BOOTSTRAP:-}" ]]; then
+    bazel build --output_groups=+types //seed/cloud/login/proto:login_ts_proto
+  fi
   cp -f ./bazel-bin/seed/cloud/login/proto/login_pb.js ./seed/cloud/login/proto/login_pb.js
   cp -f ./bazel-bin/seed/cloud/login/proto/login_pb.d.ts ./seed/cloud/login/proto/login_pb.d.ts
 fi
--- seed/devprod/golink/proto/build.sh
+++ seed/devprod/golink/proto/build.sh
@@ -6,19 +6,25 @@ cd $(dirname "${BASH_SOURCE[0]}")/../../../..
 IFS="+" target="${@:1}"
 
 if [[ "${target}" == "" || "${target}" == *"go"* ]]; then
-  bazel build --output_groups=+go_generated_srcs //seed/devprod/golink/proto:golink_go_proto
+  if [[ -z "${SEED_MONOREPO_BOOTSTRAP:-}" ]]; then
+    bazel build --output_groups=+go_generated_srcs //seed/devprod/golink/proto:golink_go_proto
+  fi
   mkdir -p ./seed/devprod/golink/proto/golinkpb
   cp -f \
     ./bazel-bin/seed/devprod/golink/proto/golink_go_proto_/github.com/ndscm/theseed/seed/devprod/golink/proto/golinkpb/golink.pb.go \
     ./seed/devprod/golink/proto/golinkpb/golink.pb.go
 
-  bazel build //seed/devprod/golink/proto:golink_go_connect_go
+  if [[ -z "${SEED_MONOREPO_BOOTSTRAP:-}" ]]; then
+    bazel build //seed/devprod/golink/proto:golink_go_connect_go
+  fi
   mkdir -p ./seed/devprod/golink/proto/golinkpbconnect
   cp -f \
     ./bazel-bin/seed/devprod/golink/proto/golinkpbconnect/golink.connect.go \
     ./seed/devprod/golink/proto/golinkpbconnect/golink.connect.go
 
-  bazel build --output_groups=+go_generated_srcs //seed/devprod/golink/proto:golink_error_go_proto
+  if [[ -z "${SEED_MONOREPO_BOOTSTRAP:-}" ]]; then
+    bazel build --output_groups=+go_generated_srcs //seed/devprod/golink/proto:golink_error_go_proto
+  fi
   mkdir -p ./seed/devprod/golink/proto/golinkerrorpb
   cp -f \
     ./bazel-bin/seed/devprod/golink/proto/golink_error_go_proto_/github.com/ndscm/theseed/seed/devprod/golink/proto/golinkerrorpb/golink_error.pb.go \
@@ -26,10 +32,14 @@ if [[ "${target}" == "" || "${target}" == *"go"* ]]; then
 fi
 
 if [[ "${target}" == "" || "${target}" == *"ts"* ]]; then
-  bazel build --output_groups=+types //seed/devprod/golink/proto:golink_ts_proto
+  if [[ -z "${SEED_MONOREPO_BOOTSTRAP:-}" ]]; then
+    bazel build --output_groups=+types //seed/devprod/golink/proto:golink_ts_proto
+  fi
   cp -f ./bazel-bin/seed/devprod/golink/proto/golink_pb.js ./seed/devprod/golink/proto/golink_pb.js
   cp -f ./bazel-bin/seed/devprod/golink/proto/golink_pb.d.ts ./seed/devprod/golink/proto/golink_pb.d.ts
-  bazel build --output_groups=+types //seed/devprod/golink/proto:golink_error_ts_proto
+  if [[ -z "${SEED_MONOREPO_BOOTSTRAP:-}" ]]; then
+    bazel build --output_groups=+types //seed/devprod/golink/proto:golink_error_ts_proto
+  fi
   cp -f ./bazel-bin/seed/devprod/golink/proto/golink_error_pb.js ./seed/devprod/golink/proto/golink_error_pb.js
   cp -f ./bazel-bin/seed/devprod/golink/proto/golink_error_pb.d.ts ./seed/devprod/golink/proto/golink_error_pb.d.ts
 fi
