93cf09b116ae5503e3f51b41aeec6ac21f5f8212
Nagi <nagi@ndscm.com>
2026-04-02 17:29:19 +0800

    seed: bootstrap: build bazel once during bootstrap
    Change-uuid: 692ae2d6-a7db-5e4a-91c9-25a7d067f879

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,21 @@ set -eux
 set -o pipefail
 cd $(dirname "${BASH_SOURCE[0]}")
 
+export SEED_MONOREPO_BOOTSTRAP=1
+
+bazel build \
+  --output_groups=+go_generated_srcs \
+  --output_groups=+types \
+  $(
+    bazel query \
+      'filter(".*:ent_full_srcs$", //...)' \
+      ' + filter(".*(_go_connect_go)$", //...)' \
+      ' + filter(".*(_go_proto)$", //...)' \
+      ' + filter(".*(_py_grpc)$", //...)' \
+      ' + filter(".*(_py_pb2)$", //...)' \
+      ' + filter(".*(_ts_proto)$", //...)'
+  )
+
 # Monorepo
 export ELECTRON_GET_USE_PROXY=1
 bazel run @pnpm//:pnpm -- --dir $PWD install
