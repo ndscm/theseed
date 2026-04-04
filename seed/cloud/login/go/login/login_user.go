779591129e3a06097d8e9c341987103d45d0515d
Nagi <nagi@ndscm.com>
2026-04-04 15:07:34 +0800

    seed: login: add ensure login helper
    Change-uuid: b57bf5c2-f784-5a5a-865a-1584fcf70be4

--- seed/cloud/login/go/login/login_user.go
+++ seed/cloud/login/go/login/login_user.go
@@ -14,3 +14,14 @@ func LoginUser(ctx context.Context) (*seedjwt.OpenidUserInfo, error) {
 	}
 	return jwtUser, nil
 }
+
+func EnsureLoginUser(ctx context.Context) (*seedjwt.OpenidUserInfo, error) {
+	loginUser, err := LoginUser(ctx)
+	if err != nil {
+		return nil, seederr.Wrap(err)
+	}
+	if loginUser == nil || loginUser.Sub == "" {
+		return nil, seederr.WrapErrorf("user not logged in")
+	}
+	return loginUser, nil
+}
