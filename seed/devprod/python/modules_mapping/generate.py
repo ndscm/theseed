031bd8dcc9361cbef0ae468c7cb793a8ad000193
Nagi <nagi@ndscm.com>
2026-04-13 02:31:27 +0800

    seed: python: move modules mapping to workspace root
    modules mapping files should work like any other lock files
    
    Change-uuid: ac78d99e-8655-40fa-b34f-dbb22bcf2248

--- seed/devprod/python/modules_mapping/generate.py
+++ seed/devprod/python/modules_mapping/generate.py
@@ -11,12 +11,18 @@ async def main():
     build_workspace_directory = os.getenv("BUILD_WORKSPACE_DIRECTORY", "").strip()
     if not build_workspace_directory:
         raise ValueError("must run with bazel")
-    with open("seed/devprod/python/modules_mapping/local_modules_mapping.json", "r") as f:
+    with open(
+        "seed/devprod/python/modules_mapping/local_modules_mapping.json", "r"
+    ) as f:
         mapping = json.load(f)
     if platform.system() == "Linux":
-        output = f"{build_workspace_directory}/seed/devprod/python/modules_mapping/modules_mapping_linux.json"
+        output = (
+            f"{build_workspace_directory}/gazelle_python_modules_mapping_linux.json"
+        )
     elif platform.system() == "Darwin":
-        output = f"{build_workspace_directory}/seed/devprod/python/modules_mapping/modules_mapping_darwin.json"
+        output = (
+            f"{build_workspace_directory}/gazelle_python_modules_mapping_darwin.json"
+        )
     else:
         raise ValueError(f"not supported platform: {platform.system()}")
     with open(output, "w") as f:
