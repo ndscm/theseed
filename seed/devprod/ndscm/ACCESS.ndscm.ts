0247d3f6096768d73f2ef5907a87223d21c69442
Nagi <nagi@ndscm.com>
2026-04-15 00:42:20 +0800

    seed: ndscm: add project access config
    Change-uuid: 042932e2-0086-435e-9e78-5b62a0ee599d
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/devprod/ndscm/ACCESS.ndscm.ts
@@ -0,0 +1,15 @@
+import { type AccessConfig } from "@//seed/devprod/ndscm/config/ACCESS"
+
+export default {
+  owner: "nagi",
+  maintainer: [
+    // sort
+    "charlie",
+  ],
+  reviewer: [
+    // sort
+    "charlie",
+    "christina",
+    "nagi",
+  ],
+} satisfies AccessConfig
