4a8d0d44d68a5484471d10ee288f649463d527f0
Nagi <nagi@ndscm.com>
2026-04-15 03:13:18 +0800

    seed: ndscm: wrap user package
    Change-uuid: 5a3a935c-477a-4ff6-baea-f72496b7a3ee

--- seed/devprod/ndscm/common/config.go
+++ seed/devprod/ndscm/common/config.go
@@ -14,14 +14,12 @@ var flagScm = seedflag.DefineString("scm", "git", "the scm backend")
 
 var flagMonorepoHome = seedflag.DefineString("monorepo_home", "", "the monorepo home directory")
 var flagMonorepoGitDir = seedflag.DefineString("monorepo_git_dir", "", "the monorepo git directory")
-var flagUserHandle = seedflag.DefineString("user_handle", "", "the user handle")
 
 type NdConfig struct {
 	MonorepoGitDir string
 	MonorepoHome   string
 	MountHome      string // Reserved for single mount point
 	Scm            string
-	UserHandle     string
 }
 
 func LoadConfig() (*NdConfig, error) {
@@ -47,13 +45,11 @@ func LoadConfig() (*NdConfig, error) {
 		}
 		monorepoGitDir = filepath.Join(homeDir, monorepoGitDir[2:])
 	}
-	userHandle := flagUserHandle.Get()
 	ndConfig := &NdConfig{
 		MountHome:      "",
 		MonorepoHome:   monorepoHome,
 		MonorepoGitDir: monorepoGitDir,
 		Scm:            "",
-		UserHandle:     userHandle,
 	}
 	ndConfig.Scm = flagScm.Get()
 	if len(ndConfig.MonorepoHome) == 0 {
@@ -61,9 +57,6 @@ func LoadConfig() (*NdConfig, error) {
 	}
 	if len(ndConfig.MonorepoGitDir) == 0 {
 		return nil, seederr.WrapErrorf("ND_MONOREPO_GIT_DIR is not set")
-	}
-	if len(ndConfig.UserHandle) == 0 {
-		return nil, seederr.WrapErrorf("ND_USER_HANDLE is not set")
 	}
 	return ndConfig, nil
 }
