ef908012170f04cc108250d1d1544ec80b27ea1b
Nagi <nagi@ndscm.com>
2026-04-23 00:26:28 +0800

    seed: hooin: add start container script
    Change-uuid: 56fb3260-c4bb-40bf-ba22-347f6a0f652f

---
+++ seed/newtype/hooin/container/start-on-premise.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+cd $(dirname "${BASH_SOURCE[0]}")
+
+server=${1:-"newtype.ndscm.com"}
+mount_etc_hooin="${2:-"/mnt/data/hooin/etc/hooin"}"
+
+./build.sh
+"${CONTAINER_CLI}" save ghcr.io/ndscm/hooin:latest | ssh "${server}" "${CONTAINER_CLI} load"
+
+printf "Use this command to access the container:\n"
+printf "    \x1b[1;33m${CONTAINER_CLI} --host \"ssh://${server}\" exec --interactive --tty --user hooin hooin zsh\x1b[0m\n"
+
+"${CONTAINER_CLI}" --host "ssh://${server}" rm -f hooin || true
+"${CONTAINER_CLI}" --host "ssh://${server}" run --name hooin --interactive --tty \
+  --network=host \
+  --volume "${mount_etc_hooin}:/etc/hooin" \
+  ghcr.io/ndscm/hooin:latest
