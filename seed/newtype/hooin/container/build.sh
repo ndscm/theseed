27152d0e44e1305a006edefd8c7b4d143294cf5c
Nagi <nagi@ndscm.com>
2026-04-23 00:13:18 +0800

    seed: hooin: add container image
    Change-uuid: 22e5e144-a573-42c7-b2d1-e34ee202977f

---
+++ seed/newtype/hooin/container/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+cd $(dirname "${BASH_SOURCE[0]}")/../../../..
+
+bazel build //seed/newtype/hooin/server
+cp -f ./bazel-bin/seed/newtype/hooin/server/hooin-server_/hooin-server ./seed/newtype/hooin/container/hooin-server
+
+cd ./seed/newtype/hooin/container/
+"${CONTAINER_CLI}" build -t ghcr.io/ndscm/hooin:latest .
