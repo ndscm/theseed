6bba4e0572fd235efe3490e1026c0e4e0c916ce6
Nagi <nagi@ndscm.com>
2026-04-22 19:28:11 +0800

    seed: amadeus: wake: implement wake to commute hooin
    Change-uuid: 0648f3d9-084d-48b8-ab2c-fb3e8c535eb8

--- seed/newtype/amadeus/wake/service/wake_service.go
+++ seed/newtype/amadeus/wake/service/wake_service.go
@@ -4,16 +4,20 @@ import (
 	"context"
 
 	"connectrpc.com/connect"
+	"github.com/ndscm/theseed/seed/cloud/login/go/login"
 	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
+	"github.com/ndscm/theseed/seed/newtype/amadeus/onduty"
 	"github.com/ndscm/theseed/seed/newtype/amadeus/wake/proto/wakepb"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type AmadeusWakeService struct {
+	conscious *onduty.Conscious
 }
 
-func (svc *AmadeusWakeService) Initialize() error {
+func (svc *AmadeusWakeService) Initialize(conscious *onduty.Conscious) error {
+	svc.conscious = conscious
 	return nil
 }
 
@@ -21,7 +25,16 @@ func (svc *AmadeusWakeService) Wake(
 	ctx context.Context,
 	req *connect.Request[wakepb.WakeRequest],
 ) (*connect.Response[emptypb.Empty], error) {
-	return nil, seederr.CodeErrorf(codes.Unimplemented, "Wake is not implemented")
+	_, err := login.EnsureLoginUser(ctx)
+	if err != nil {
+		return nil, seederr.Wrap(err)
+	}
+	// TODO(nagi): add fine-grained authorization
+	err = svc.conscious.Wake(ctx, req.Msg.GetToken(), req.Msg.GetHooinDirectServer())
+	if err != nil {
+		return nil, seederr.Wrap(err)
+	}
+	return connect.NewResponse(&emptypb.Empty{}), nil
 }
 
 func (svc *AmadeusWakeService) RestartBrain(
@@ -35,5 +48,23 @@ func (svc *AmadeusWakeService) Hibernate(
 	ctx context.Context,
 	req *connect.Request[wakepb.HibernateRequest],
 ) (*connect.Response[emptypb.Empty], error) {
-	return nil, seederr.CodeErrorf(codes.Unimplemented, "Hibernate is not implemented")
+	_, err := login.EnsureLoginUser(ctx)
+	if err != nil {
+		return nil, seederr.Wrap(err)
+	}
+	// TODO(nagi): add fine-grained authorization
+	done := svc.conscious.Hibernate()
+	if done == nil {
+		return connect.NewResponse(&emptypb.Empty{}), nil
+	}
+
+	if req.Msg.GetWait() {
+		select {
+		case <-done:
+		case <-ctx.Done():
+			return nil, seederr.Wrap(ctx.Err())
+		}
+	}
+
+	return connect.NewResponse(&emptypb.Empty{}), nil
 }
