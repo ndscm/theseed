1471bba4579e3dd25c9b4331a748061df5c797f8
Nagi <nagi@ndscm.com>
2026-04-23 19:08:00 +0800

    seed: amadeus: add cmd hibernate
    Change-uuid: 86e13e13-75c7-4ba1-9f34-a05c27461cb9

---
+++ seed/newtype/amadeus/cmd/hibernate/hibernate.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
+	"github.com/ndscm/theseed/seed/infra/flag/go/seedflag"
+	"github.com/ndscm/theseed/seed/infra/init/go/initctx"
+	"github.com/ndscm/theseed/seed/infra/init/go/seedinit"
+	"github.com/ndscm/theseed/seed/infra/log/go/seedlog"
+	"github.com/ndscm/theseed/seed/newtype/amadeus/wake/client/go/wakeclient"
+)
+
+var flagWait = seedflag.DefineBool("wait", false, "Wait for hibernation to complete")
+
+func run() error {
+	_, err := seedinit.Initialize(
+		seedinit.WithFallbackEnvPrefix("SEED_"),
+	)
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+	ctx := initctx.Background()
+	client := wakeclient.NewAmadeusWakeClient("")
+	err = client.Hibernate(ctx, flagWait.Get())
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+	return nil
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		seedlog.Errorf("%v", err)
+		os.Exit(1)
+	}
+}
