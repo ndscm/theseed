f208061e8e3606ec754fd0f675fd620bdbcc3f13
Nagi <nagi@ndscm.com>
2026-04-25 18:51:57 +0800

    seed: ndscm: add scm registry
    Change-uuid: d5885032-e88a-4eaa-b09b-b5d3f3350719

--- seed/devprod/ndscm/scm/registry.go
+++ seed/devprod/ndscm/scm/registry.go
@@ -1,18 +1,90 @@
 package scm
 
 import (
+	"sync"
+
 	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
 	"github.com/ndscm/theseed/seed/infra/flag/go/seedflag"
+	"github.com/ndscm/theseed/seed/infra/log/go/seedlog"
 )
 
 // The default SCM provider is "ndscm", which is used to force the user to set git explicitly.
+// User can also provide empty value to use the default provider if there is only one provider registered.
 var flagScm = seedflag.DefineString("scm", "ndscm", "The SCM provider to use (e.g. git)")
 
 func ScmName() (string, error) {
+	// TODO(nagi): remove this flag getter after all the checks are migrated.
 	scm := flagScm.Get()
 	switch scm {
 	case "git":
 		return "git", nil
+	case "":
+		return "", nil
 	}
 	return "", seederr.WrapErrorf("scm is unsupported: %v", scm)
+}
+
+type ProviderEntry struct {
+	provider Provider
+
+	initializedMutex sync.Mutex
+	initialized      bool
+}
+
+func (e *ProviderEntry) Initialize() error {
+	e.initializedMutex.Lock()
+	defer e.initializedMutex.Unlock()
+	if e.initialized {
+		return seederr.WrapErrorf("the scm is already initialized")
+	}
+	err := e.provider.Initialize()
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+	e.initialized = true
+	return nil
+}
+
+var scmRegistryMutex sync.Mutex
+var scmRegistry = map[string]*ProviderEntry{}
+
+func Register(identifier string, provider Provider) {
+	scmRegistryMutex.Lock()
+	defer scmRegistryMutex.Unlock()
+	scmRegistry[identifier] = &ProviderEntry{provider: provider}
+}
+
+func getDefaultProviderEntry() (*ProviderEntry, error) {
+	scmRegistryMutex.Lock()
+	defer scmRegistryMutex.Unlock()
+	if len(scmRegistry) == 0 {
+		return nil, seederr.WrapErrorf("no scm provider is registered")
+	}
+	scmIdentifier := flagScm.Get()
+	if scmIdentifier == "" {
+		if len(scmRegistry) != 1 {
+			return nil, seederr.WrapErrorf("multiple scm providers are registered, but no scm flag is set")
+		}
+		for k := range scmRegistry {
+			scmIdentifier = k
+		}
+		seedlog.Infof("SCM: %s", scmIdentifier)
+	}
+	entry, ok := scmRegistry[scmIdentifier]
+	if !ok {
+		return nil, seederr.WrapErrorf("SCM provider not found: %s", scmIdentifier)
+	}
+	return entry, nil
+}
+
+func InitializeDefaultProvider() (Provider, error) {
+	entry, err := getDefaultProviderEntry()
+	if err != nil {
+		return nil, seederr.Wrap(err)
+	}
+	err = entry.Initialize()
+	if err != nil {
+		return nil, seederr.Wrap(err)
+	}
+	return entry.provider, nil
 }
