fbbb350ead2930b80acdfc915d68d8b195d5b160
Nagi <nagi@ndscm.com>
2026-04-26 02:27:20 +0800

    seed: ndscm: remove scm flag check in each cli command
    Unknown scm flag should fail during getting default scm provider
    
    Change-uuid: 6e5fa4e8-51fd-4378-914f-7d8d456d9c6d

--- seed/devprod/ndscm/clientcore/nd_dev.go
+++ seed/devprod/ndscm/clientcore/nd_dev.go
@@ -14,56 +14,47 @@ func NdDev(scmProvider scm.Provider, args []string) error {
 	if !seedshell.ShellEval() {
 		log.Printf("\x1b[33mWarning: It's recommended to run nd-dev with --shell-eval\x1b[0m")
 	}
-	scmName, err := scm.GetIdentifier(scmProvider)
+	monorepoHome, err := scm.MonorepoHome()
 	if err != nil {
 		return seederr.Wrap(err)
 	}
-	switch scmName {
-	case "git":
-		monorepoHome, err := scm.MonorepoHome()
+	err = scmProvider.QuickVerifyMonorepo()
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+	branchName := "dev"
+	if len(args) == 0 {
+		// pass
+	} else if len(args) == 1 {
+		branchName = "dev-" + args[0]
+	} else {
+		return seederr.WrapErrorf("nd-dev usage: nd dev [<feature-name>|<index>]")
+	}
+	worktreePath := scmProvider.GetBranchWorktree(monorepoHome, branchName)
+	worktreeStat, err := os.Stat(worktreePath)
+	if err != nil && !os.IsNotExist(err) {
+		return seederr.WrapErrorf("failed to stat worktree %v: %v", worktreePath, err)
+	}
+	if err == nil && !worktreeStat.IsDir() {
+		return seederr.WrapErrorf("worktree %v exists and is not a dir", worktreePath)
+	}
+	if os.IsNotExist(err) {
+		newWorktreePath, err := scmProvider.CreateDevWorktree(monorepoHome, branchName)
 		if err != nil {
-			return seederr.Wrap(err)
+			return seederr.WrapErrorf("failed to add branch worktree %v: %v", branchName, err)
 		}
-		err = scmProvider.QuickVerifyMonorepo()
-		if err != nil {
-			return seederr.Wrap(err)
+		if newWorktreePath != worktreePath {
+			return seederr.WrapErrorf("unexpected new worktree path: %v (expected: %v)", newWorktreePath, worktreePath)
 		}
-		branchName := "dev"
-		if len(args) == 0 {
-			// pass
-		} else if len(args) == 1 {
-			branchName = "dev-" + args[0]
-		} else {
-			return seederr.WrapErrorf("nd-dev usage: nd dev [<feature-name>|<index>]")
+	}
+	shellEval := fmt.Sprintf("\ncd \"%v\"\n", worktreePath)
+	if seedshell.Dry() {
+		log.Printf("Shell eval: %v", shellEval)
+	}
+	if seedshell.ShellEval() {
+		if !seedshell.Dry() {
+			fmt.Printf("%v", shellEval)
 		}
-		worktreePath := scmProvider.GetBranchWorktree(monorepoHome, branchName)
-		worktreeStat, err := os.Stat(worktreePath)
-		if err != nil && !os.IsNotExist(err) {
-			return seederr.WrapErrorf("failed to stat worktree %v: %v", worktreePath, err)
-		}
-		if err == nil && !worktreeStat.IsDir() {
-			return seederr.WrapErrorf("worktree %v exists and is not a dir", worktreePath)
-		}
-		if os.IsNotExist(err) {
-			newWorktreePath, err := scmProvider.CreateDevWorktree(monorepoHome, branchName)
-			if err != nil {
-				return seederr.WrapErrorf("failed to add branch worktree %v: %v", branchName, err)
-			}
-			if newWorktreePath != worktreePath {
-				return seederr.WrapErrorf("unexpected new worktree path: %v (expected: %v)", newWorktreePath, worktreePath)
-			}
-		}
-		shellEval := fmt.Sprintf("\ncd \"%v\"\n", worktreePath)
-		if seedshell.Dry() {
-			log.Printf("Shell eval: %v", shellEval)
-		}
-		if seedshell.ShellEval() {
-			if !seedshell.Dry() {
-				fmt.Printf("%v", shellEval)
-			}
-		}
-	default:
-		return seederr.WrapErrorf("nd-dev does not support %v", scmName)
 	}
 	return nil
 }
--- seed/devprod/ndscm/clientcore/nd_review.go
+++ seed/devprod/ndscm/clientcore/nd_review.go
@@ -22,77 +22,68 @@ func NdReview(scmProvider scm.Provider, args []string) error {
 	if currentUserHandle == "" {
 		return seederr.WrapErrorf("user handle is not set")
 	}
-	scmName, err := scm.GetIdentifier(scmProvider)
+	monorepoHome, err := scm.MonorepoHome()
 	if err != nil {
 		return seederr.Wrap(err)
 	}
-	switch scmName {
-	case "git":
-		monorepoHome, err := scm.MonorepoHome()
-		if err != nil {
-			return seederr.Wrap(err)
-		}
-		err = scmProvider.QuickVerifyMonorepo()
-		if err != nil {
-			return seederr.Wrap(err)
-		}
-		featureName := strings.TrimSpace(args[0])
-		reviewBranch := "review/" + featureName
-		changeBranch := "change/" + featureName
-		worktreePath := scmProvider.GetBranchWorktree(monorepoHome, reviewBranch)
-		_, err = os.Stat(worktreePath)
-		if err != nil && !os.IsNotExist(err) {
-			return seederr.Wrap(err)
-		}
-		if err == nil {
-			log.Printf("Worktree %v already exists, removing...\n", worktreePath)
-			err = scmProvider.RemoveWorktree(worktreePath)
-			if err != nil {
-				return seederr.Wrap(err)
-			}
-		}
-		_, err = scmProvider.GetCommitId(reviewBranch)
-		if err == nil {
-			log.Printf("Branch %v already exists, removing...\n", reviewBranch)
-			err = scmProvider.DeleteBranch(reviewBranch)
-			if err != nil {
-				return seederr.Wrap(err)
-			}
-		}
-		trackingBranch, err := scmProvider.GetBranchTracking(changeBranch)
-		if err != nil {
-			return seederr.WrapErrorf("tracking upstream is missing for %v", changeBranch)
-		}
-		mergeBaseCommitId, err := scmProvider.GetMergeBaseCommitId("origin/main", changeBranch)
-		if err != nil {
-			return seederr.WrapErrorf("merge base is missing for %v", changeBranch)
-		}
-		err = scmProvider.CreateBranch(reviewBranch, mergeBaseCommitId, "origin/main")
-		if err != nil {
-			return seederr.Wrap(err)
-		}
-		worktreePath, err = scmProvider.CreateBranchWorktree(monorepoHome, reviewBranch)
-		if err != nil {
-			return seederr.Wrap(err)
-		}
-		err = scmProvider.ApplyCommitRange(worktreePath, trackingBranch, changeBranch)
-		if err != nil {
-			return seederr.Wrap(err)
-		}
-		err = scmProvider.PushBranch(reviewBranch, "origin", currentUserHandle+"/"+featureName)
-		if err != nil {
-			return seederr.Wrap(err)
-		}
+	err = scmProvider.QuickVerifyMonorepo()
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+	featureName := strings.TrimSpace(args[0])
+	reviewBranch := "review/" + featureName
+	changeBranch := "change/" + featureName
+	worktreePath := scmProvider.GetBranchWorktree(monorepoHome, reviewBranch)
+	_, err = os.Stat(worktreePath)
+	if err != nil && !os.IsNotExist(err) {
+		return seederr.Wrap(err)
+	}
+	if err == nil {
+		log.Printf("Worktree %v already exists, removing...\n", worktreePath)
 		err = scmProvider.RemoveWorktree(worktreePath)
 		if err != nil {
 			return seederr.Wrap(err)
 		}
+	}
+	_, err = scmProvider.GetCommitId(reviewBranch)
+	if err == nil {
+		log.Printf("Branch %v already exists, removing...\n", reviewBranch)
 		err = scmProvider.DeleteBranch(reviewBranch)
 		if err != nil {
 			return seederr.Wrap(err)
 		}
-	default:
-		return seederr.WrapErrorf("nd-review does not support %v", scmName)
+	}
+	trackingBranch, err := scmProvider.GetBranchTracking(changeBranch)
+	if err != nil {
+		return seederr.WrapErrorf("tracking upstream is missing for %v", changeBranch)
+	}
+	mergeBaseCommitId, err := scmProvider.GetMergeBaseCommitId("origin/main", changeBranch)
+	if err != nil {
+		return seederr.WrapErrorf("merge base is missing for %v", changeBranch)
+	}
+	err = scmProvider.CreateBranch(reviewBranch, mergeBaseCommitId, "origin/main")
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+	worktreePath, err = scmProvider.CreateBranchWorktree(monorepoHome, reviewBranch)
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+	err = scmProvider.ApplyCommitRange(worktreePath, trackingBranch, changeBranch)
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+	err = scmProvider.PushBranch(reviewBranch, "origin", currentUserHandle+"/"+featureName)
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+	err = scmProvider.RemoveWorktree(worktreePath)
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+	err = scmProvider.DeleteBranch(reviewBranch)
+	if err != nil {
+		return seederr.Wrap(err)
 	}
 	return nil
 }
