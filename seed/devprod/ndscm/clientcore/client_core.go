f21683e51a102dc07093fc47b4232e0afe0f3e46
Nagi <nagi@ndscm.com>
2026-04-25 21:12:46 +0800

    seed: ndscm: move nd setup command to clientcore
    Change-uuid: 9d720cd8-05d7-40aa-83cf-ce53e2ead3ed

--- seed/devprod/ndscm/clientcore/client_core.go
+++ seed/devprod/ndscm/clientcore/client_core.go
@@ -3,6 +3,10 @@ package clientcore
 type ClientCore struct {
 }
 
+func (cc *ClientCore) NdSetup(args []string) error {
+	return NdSetup(args)
+}
+
 func (cc *ClientCore) NdShell(args []string) error {
 	return NdShell(args)
 }
