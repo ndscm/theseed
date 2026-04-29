1000a415995eee8be747a9f7291647885ed3aeef
Nagi <nagi@ndscm.com>
2026-04-29 02:33:59 -0700

    seed: ndscm: add change command
    Change-uuid: fefefdeb-41b1-475e-b874-23f7187f0932

--- seed/devprod/ndscm/cli/ndscm.go
+++ seed/devprod/ndscm/cli/ndscm.go
@@ -28,6 +28,11 @@ func run() error {
 	}
 	command := args[0]
 	switch command {
+	case "change":
+		err := ndChange(args[1:])
+		if err != nil {
+			return seederr.Wrap(err)
+		}
 	case "cut":
 		err := ndCut(args[1:])
 		if err != nil {
