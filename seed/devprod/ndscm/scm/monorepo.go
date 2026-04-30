c9e51e26b991883cfd973272019c8ba67dd11956
Nagi <nagi@ndscm.com>
2026-04-30 10:18:10 -0700

    seed: ndscm: add nd connect command
    Change-uuid: c72359f0-1ea2-45bf-81d4-910c8921387a

--- seed/devprod/ndscm/scm/monorepo.go
+++ seed/devprod/ndscm/scm/monorepo.go
@@ -9,7 +9,20 @@ import (
 	"github.com/ndscm/theseed/seed/infra/flag/go/seedflag"
 )
 
+var flagReposHome = seedflag.DefineString("repos_home", "", "Repos home directory")
 var flagMonorepoHome = seedflag.DefineString("monorepo_home", "", "the monorepo home directory")
+
+func ReposHome() (string, error) {
+	reposHome := flagReposHome.Get()
+	if strings.HasPrefix(reposHome, "~/") {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return "", seederr.Wrap(err)
+		}
+		reposHome = filepath.Join(homeDir, reposHome[2:])
+	}
+	return reposHome, nil
+}
 
 func MonorepoHome() (string, error) {
 	monorepoHome := flagMonorepoHome.Get()
