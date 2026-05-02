6ac6ab1132e4a07ceb10e80328f51b442d1caa34
Nagi <nagi@ndscm.com>
2026-05-02 01:11:48 -0700

    seed: hooin: load configs from etc steins
    Change-uuid: bab152b1-a53b-42c6-a46b-c509bbb26732
    Break: load hooin config from /etc/hooin
    Migrate: move hooin configs to /etc/steins

--- seed/newtype/hooin/container/start-on-premise.sh
+++ seed/newtype/hooin/container/start-on-premise.sh
@@ -4,7 +4,7 @@ set -o pipefail
 cd $(dirname "${BASH_SOURCE[0]}")
 
 server=${1:-"newtype.ndscm.com"}
-mount_etc_hooin="${2:-"/mnt/data/hooin/etc/hooin"}"
+mount_etc_steins=${2:-"/mnt/data/steins/etc/steins"}
 
 ./build.sh
 "${CONTAINER_CLI}" save ghcr.io/ndscm/hooin:latest | ssh "${server}" "${CONTAINER_CLI} load"
@@ -15,5 +15,5 @@ printf "    \x1b[1;33m${CONTAINER_CLI} --host \"ssh://${server}\" exec --interac
 "${CONTAINER_CLI}" --host "ssh://${server}" rm -f hooin || true
 "${CONTAINER_CLI}" --host "ssh://${server}" run --name hooin --interactive --tty \
   --network=host \
-  --volume "${mount_etc_hooin}:/etc/hooin" \
+  --volume "${mount_etc_steins}:/etc/steins" \
   ghcr.io/ndscm/hooin:latest
