aa2b5a220a4d565adf961b63600dfb79ef285768
Nagi <nagi@ndscm.com>
2026-05-03 14:43:53 -0700

    seed: amadeus: build ndscm
    Change-uuid: ea9137a6-1f46-4422-93e9-c32c3b5adf72
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/amadeus/container/build.sh
+++ seed/newtype/amadeus/container/build.sh
@@ -3,6 +3,9 @@ set -eux
 set -o pipefail
 cd $(dirname "${BASH_SOURCE[0]}")/../../../..
 
+bazel build //seed/devprod/ndscm/cli
+cp -f ./bazel-bin/seed/devprod/ndscm/cli/ndscm_/ndscm ./seed/newtype/amadeus/container/ndscm
+
 bazel build //seed/newtype/amadeus/server
 cp -f ./bazel-bin/seed/newtype/amadeus/server/amadeus-server_/amadeus-server ./seed/newtype/amadeus/container/amadeus-server
 
