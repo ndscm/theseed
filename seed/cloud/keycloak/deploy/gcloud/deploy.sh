6bd02acc3623839c784008f8fcdb840761125c33
Nagi <nagi@ndscm.com>
2026-05-02 19:45:27 -0700

    seed: keycloak: gcloud: deploy keycloak to gcloud run
    Change-uuid: 1c210c70-d180-41a6-9be3-8ec86f119702

---
+++ seed/cloud/keycloak/deploy/gcloud/deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")/../../../../.."
+
+container_cli=${CONTAINER_CLI:-"docker"}
+
+project="ndscm-prod"
+region="us-west1"
+service="seed-cloud-keycloak-prod"
+image_package="us-docker.pkg.dev/ndscm-prod/container-us/seed-cloud-keycloak-deploy-gcloud"
+
+./seed/cloud/keycloak/container/build.sh
+
+cd ./seed/cloud/keycloak/deploy/gcloud/
+"${container_cli}" build -t "${image_package}:prod" .
+
+"${container_cli}" push "${image_package}:prod"
+
+image_digest="$("${container_cli}" inspect --format='{{index .RepoDigests 0}}' "${image_package}:prod")"
+gcloud run services update "${service}" \
+  --project="${project}" \
+  --region="${region}" \
+  --image="${image_digest}" \
+  --no-cpu-throttling \
+  --execution-environment gen2
