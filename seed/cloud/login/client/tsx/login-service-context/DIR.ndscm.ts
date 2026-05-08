7b4d5a3b65c3583a30f85429947dd7c5de332ef5
Nagi <nagi@ndscm.com>
2026-05-08 10:13:39 -0700

    seed: login: add ndscm dir configs
    Change-uuid: 323970d3-11b9-43bc-960c-8d1d8465e1c0
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/cloud/login/client/tsx/login-service-context/DIR.ndscm.ts
@@ -0,0 +1,18 @@
+import { type DirConfig } from "../../../../../devprod/ndscm/config/DIR"
+
+export default {
+  bootstrap: {
+    node_modules: {
+      target: "node_modules",
+      watch: "^package.json$",
+      run: 'bazel run @pnpm//:pnpm -- --dir "$(pwd)" install',
+    },
+  },
+  build: {
+    dist: {
+      target: "dist",
+      watch: "^src/",
+      run: 'bazel run @pnpm//:pnpm -- --dir "$(pwd)" build',
+    },
+  },
+} satisfies DirConfig
