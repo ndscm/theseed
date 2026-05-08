1f4a398259bb35e2c20339051cc02c67f6c6d42a
Nagi <nagi@ndscm.com>
2026-05-08 10:16:19 -0700

    seed: stuff: add ndscm dir configs
    Change-uuid: 611a8868-94f9-4abc-a897-81d627f1f2d5
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/office/stuff/proto/DIR.ndscm.ts
@@ -0,0 +1,19 @@
+import { type DirConfig } from "../../../devprod/ndscm/config/DIR"
+
+export default {
+  bootstrap: {
+    proto: {
+      target: [
+        "stuffpb",
+        "stuffpbconnect",
+        "stufferrorpb",
+        "stuff_pb.js",
+        "stuff_pb.d.ts",
+        "stuff_error_pb.js",
+        "stuff_error_pb.d.ts",
+      ],
+      watch: "\\.proto$",
+      run: "./build.sh",
+    },
+  },
+} satisfies DirConfig
