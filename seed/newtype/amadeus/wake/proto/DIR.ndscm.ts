7437ef04cf91d5cb98cce93bb54301ca1abf1f67
Nagi <nagi@ndscm.com>
2026-05-08 10:15:38 -0700

    seed: amadeus: add ndscm dir configs
    Change-uuid: dd105e8e-cfd3-439a-94e4-fd60a1b61c54
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/newtype/amadeus/wake/proto/DIR.ndscm.ts
@@ -0,0 +1,15 @@
+import { type DirConfig } from "../../../../devprod/ndscm/config/DIR"
+
+export default {
+  bootstrap: {
+    proto: {
+      target: [
+        //
+        "wakepb",
+        "wakepbconnect",
+      ],
+      watch: "\\.proto$",
+      run: "./build.sh",
+    },
+  },
+} satisfies DirConfig
