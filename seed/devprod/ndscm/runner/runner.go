fa7884ade12f28154a791f20703ebb29d526affa
Nagi <nagi@ndscm.com>
2026-05-08 01:24:17 -0700

    seed: ndscm: run format with runner
    Change-uuid: d8fc1294-f842-42d6-a8b3-3980d9454b23
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/ndscm/runner/runner.go
+++ seed/devprod/ndscm/runner/runner.go
@@ -1,5 +1,11 @@
 package runner
 
+import (
+	"slices"
+
+	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
+)
+
 type Runner struct {
 	worktree string
 
@@ -8,6 +14,21 @@ type Runner struct {
 	scmChangePaths []string
 
 	scmDirtyPaths []string
+}
+
+func (r *Runner) Format(all bool) error {
+	if !all {
+		err := FormatDirtyFiles(r.worktree, r.scmFilePaths, slices.Concat(r.scmChangePaths, r.scmDirtyPaths))
+		if err != nil {
+			return seederr.Wrap(err)
+		}
+	} else {
+		err := FormatAllFiles(r.worktree, r.scmFilePaths)
+		if err != nil {
+			return seederr.Wrap(err)
+		}
+	}
+	return nil
 }
 
 func CreateRunner(worktree string, scmFilePaths []string, scmChangePaths []string, scmDirtyPaths []string) (*Runner, error) {
