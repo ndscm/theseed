166887746b007ddce4de7a89ff8a8b06cc5d410a
Nagi <nagi@ndscm.com>
2026-05-09 02:31:57 -0700

    seed: ndscm: prebuild bazel targets and dispatch format in go routines
    Change-uuid: 12967671-1fae-49f9-ac39-09ca4b6a6396
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/ndscm/runner/format.go
+++ seed/devprod/ndscm/runner/format.go
@@ -1,14 +1,17 @@
 package runner
 
 import (
+	"errors"
 	"os/exec"
 	"path/filepath"
 	"sort"
 	"strings"
+	"sync"
 
 	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
 	"github.com/ndscm/theseed/seed/infra/log/go/seedlog"
 	"github.com/ndscm/theseed/seed/infra/shell/go/seedshell"
+	"golang.org/x/sync/errgroup"
 )
 
 var bashEscapeReplacer = strings.NewReplacer(
@@ -22,9 +25,49 @@ func escapeFilePathForBash(filePath string) string {
 	return bashEscapeReplacer.Replace(filePath)
 }
 
+func formatFile(
+	worktreeAbsPath string, scmTargetPath string, watchers []Watcher, bazelGround *BazelGround,
+) error {
+	targetAbsPath := filepath.Join(worktreeAbsPath, scmTargetPath)
+	escapedTarget := escapeFilePathForBash(targetAbsPath)
+	for _, watcher := range watchers {
+		for _, runTask := range watcher.Run {
+			dirAbsPath := filepath.Join(worktreeAbsPath, runTask.DirPath)
+			bashCmd := runTask.Cmd
+			bashCmd = strings.ReplaceAll(bashCmd, "{{TARGET}}", escapedTarget)
+			bashCmd, err := bazelGround.fulfill(bashCmd, runTask.BazelTargets)
+			if err != nil {
+				return seederr.Wrap(err)
+			}
+			err = seedshell.ImpureOptionsRun(
+				[]seedshell.RunOption{func(cmd *exec.Cmd) {
+					cmd.Dir = dirAbsPath
+
+					// Comfort the aspect js_binary rule
+					cmd.Env = append(cmd.Env, "BAZEL_BINDIR=.")
+				}},
+				"bash", "-c", bashCmd)
+			if err != nil {
+				return seederr.Wrap(err)
+			}
+		}
+	}
+	return nil
+}
+
 func formatFiles(worktree string, formatPhase RepoPhase, filePaths []string) error {
 	seedlog.Debugf("Formatting files:\n%v", strings.Join(filePaths, "\n"))
 	worktreeAbsPath, err := filepath.Abs(worktree)
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+
+	bazelGround := NewBazelGround()
+	err = bazelGround.collect(formatPhase)
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+	err = bazelGround.build(worktree)
 	if err != nil {
 		return seederr.Wrap(err)
 	}
@@ -40,6 +83,11 @@ func formatFiles(worktree string, formatPhase RepoPhase, filePaths []string) err
 	}
 	sort.Strings(scmTargetPaths)
 
+	errGroup := errgroup.Group{}
+	errGroup.SetLimit(100)
+	errsMutex := sync.Mutex{}
+	errs := []error{}
+
 	for _, scmTargetPath := range scmTargetPaths {
 		watchers := formatPhase.Targets[scmTargetPath]
 		hasDirty := false
@@ -54,27 +102,23 @@ func formatFiles(worktree string, formatPhase RepoPhase, filePaths []string) err
 				break
 			}
 		}
-		if hasDirty {
-			targetAbsPath := filepath.Join(worktreeAbsPath, scmTargetPath)
-			escapedTarget := escapeFilePathForBash(targetAbsPath)
-			for _, watcher := range watchers {
-				for _, runTask := range watcher.Run {
-					dirAbsPath := filepath.Join(worktreeAbsPath, runTask.DirPath)
-					bashCmd := runTask.Cmd
-					bashCmd = strings.ReplaceAll(bashCmd, "{{TARGET}}", escapedTarget)
-					err := seedshell.ImpureOptionsRun(
-						[]seedshell.RunOption{func(cmd *exec.Cmd) {
-							cmd.Dir = dirAbsPath
-						}},
-						"bash", "-c", bashCmd)
-					if err != nil {
-						return seederr.Wrap(err)
-					}
-				}
-			}
+		if !hasDirty {
+			continue
 		}
+		errGroup.Go(func() error {
+			err := formatFile(worktreeAbsPath, scmTargetPath, watchers, bazelGround)
+			if err != nil {
+				errsMutex.Lock()
+				defer errsMutex.Unlock()
+				errs = append(errs, err)
+			}
+			return nil
+		})
 	}
-	return nil
+	errGroup.Wait()
+
+	seedlog.Infof("Finished formatting files: count=%d", len(scmTargetPaths))
+	return errors.Join(errs...)
 }
 
 func FormatDirtyFiles(worktree string, scmFilePaths []string, scmDirtyPaths []string) error {
