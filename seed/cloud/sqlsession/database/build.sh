da49a85d7dfebaadfe7eaa36585bd40e73f28acf
Nagi <nagi@ndscm.com>
2026-05-10 08:03:31 -0700

    seed: sqlsession: add bootstrap bazel rules
    Change-uuid: 8606e914-4f88-4476-a43f-d1da4eb4beaf
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/cloud/sqlsession/database/build.sh
+++ seed/cloud/sqlsession/database/build.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 set -o pipefail
-cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
-if [[ -z "${SEED_MONOREPO_BOOTSTRAP:-}" ]]; then
-  bazel build //seed/cloud/sqlsession/database:ent_full_srcs
-fi
-mkdir -p ./seed/cloud/sqlsession/database/ent/
-rsync --archive --delete \
-  --include='*/' --include='*.go' --exclude='*' \
-  ./bazel-bin/seed/cloud/sqlsession/database/ent/. \
-  ./seed/cloud/sqlsession/database/ent/.
+# Convenience wrapper for running the bootstrap rule directly.
+bazel run :bootstrap
