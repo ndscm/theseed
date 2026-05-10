6535fefdc28e51b88666a9b63fac57b31d0cce9b
Nagi <nagi@ndscm.com>
2026-05-10 07:34:58 -0700

    seed: gajetto: add bootstrap bazel rules
    Change-uuid: 23372724-a941-4679-b2cc-048e18fe0927
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/gajetto/proto/build.sh
+++ seed/newtype/gajetto/proto/build.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 set -o pipefail
-cd $(dirname "${BASH_SOURCE[0]}")/../../../..
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
-IFS="+" target="${@:1}"
-
-if [[ "${target}" == "" || "${target}" == *"go"* ]]; then
-  if [[ -z "${SEED_MONOREPO_BOOTSTRAP:-}" ]]; then
-    bazel build --output_groups=+go_generated_srcs //seed/newtype/gajetto/proto:brain_go_proto
-  fi
-  mkdir -p ./seed/newtype/gajetto/proto/brainpb
-  cp -f \
-    ./bazel-bin/seed/newtype/gajetto/proto/brain_go_proto_/github.com/ndscm/theseed/seed/newtype/gajetto/proto/brainpb/brain.pb.go \
-    ./seed/newtype/gajetto/proto/brainpb/brain.pb.go
-fi
+# Convenience wrapper for running the bootstrap rule directly.
+bazel run :bootstrap
