fce68495e7f29bed1e0be1323cdda7ed1f258e22
Nagi <nagi@ndscm.com>
2026-05-13 07:36:04 -0700

    seed: golink: watch monorepo node modules
    Change-uuid: d3f1698d-38d8-464d-a5ac-b6a92d2e9d15
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/golink/client/tsx/golink-service-context/DIR.ndscm.ts
+++ seed/devprod/golink/client/tsx/golink-service-context/DIR.ndscm.ts
@@ -2,14 +2,9 @@ import { type DirConfig } from "../../../../ndscm/config/DIR"
 
 export default {
   bootstrap: {
-    node_modules: {
-      target: "node_modules",
-      watch: "^package.json$",
-      run: 'bazel run @pnpm//:pnpm -- --dir "$(pwd)" install',
-    },
     dist: {
       target: "dist",
-      watchRepo: "seed/devprod/golink/proto/golink_pb.js",
+      watchRepo: ["node_modules", "seed/devprod/golink/proto/golink_pb.js"],
       watch: "^src/",
       run: 'bazel run @pnpm//:pnpm -- --dir "$(pwd)" build',
     },
--- seed/devprod/golink/webapp/DIR.ndscm.ts
+++ seed/devprod/golink/webapp/DIR.ndscm.ts
@@ -1,16 +1,10 @@
 import { type DirConfig } from "../../ndscm/config/DIR"
 
 export default {
-  bootstrap: {
-    node_modules: {
-      target: "node_modules",
-      watch: "^package.json$",
-      run: 'bazel run @pnpm//:pnpm -- --dir "$(pwd)" install',
-    },
-  },
   build: {
     dist: {
       target: "dist",
+      watchRepo: ["node_modules"],
       watch: "\\.(ts|tsx|js|jsx|json|css|html)$",
       run: 'bazel run @pnpm//:pnpm -- --dir "$(pwd)" build',
     },
