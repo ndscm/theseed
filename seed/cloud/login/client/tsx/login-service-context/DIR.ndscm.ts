28f66271307c15d7f001fafd6acb4060531d7d4f
Nagi <nagi@ndscm.com>
2026-05-13 07:36:39 -0700

    seed: login: watch monorepo node modules
    Change-uuid: 9a0a3fbb-6623-41f4-8581-de8a5ad23d7b
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/cloud/login/client/tsx/login-service-context/DIR.ndscm.ts
+++ seed/cloud/login/client/tsx/login-service-context/DIR.ndscm.ts
@@ -2,14 +2,9 @@ import { type DirConfig } from "../../../../../devprod/ndscm/config/DIR"
 
 export default {
   bootstrap: {
-    node_modules: {
-      target: "node_modules",
-      watch: "^package.json$",
-      run: 'bazel run @pnpm//:pnpm -- --dir "$(pwd)" install',
-    },
     dist: {
       target: "dist",
-      watchRepo: "seed/cloud/login/proto/login_pb.js",
+      watchRepo: ["node_modules", "seed/cloud/login/proto/login_pb.js"],
       watch: "^src/",
       run: 'bazel run @pnpm//:pnpm -- --dir "$(pwd)" build',
     },
