bf55683ba58888d08cb0846066512dc95ba98894
Nagi <nagi@ndscm.com>
2026-05-14 22:12:44 -0700

    seed: container: debian: build container
    Change-uuid: 6abd6410-bfa4-45b1-9308-c2c14c231c9e
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/devprod/container/debian/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+container_engine=${CONTAINER_ENGINE:-"podman"}
+
+"${container_engine}" build -f ./Containerfile -t ghcr.io/ndscm/debian:latest .
