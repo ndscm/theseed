e88c5c84a5e91192c7360f7233ef3eed5c1b3cb6
Nagi <nagi@ndscm.com>
2026-05-16 10:25:05 -0700

    seed: starter: rustcli: add rustcli to print ferris
    Change-uuid: 647ec099-ecf2-4f68-9675-2f0fc2afdafb
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/devprod/starter/rustcli/src/main.rs
@@ -0,0 +1,12 @@
+use ferris_says::say;
+use std::io::stdout;
+use std::io::BufWriter;
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
+}
