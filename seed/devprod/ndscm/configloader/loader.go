48d961534d9d7628dce67ba76e4036fec3afca5d
Nagi <nagi@ndscm.com>
2026-05-18 10:05:02 -0700

    seed: ndscm: load repo config
    Change-uuid: 3305cbfd-26e4-4fe9-8648-ab97131c7e05
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/ndscm/configloader/loader.go
+++ seed/devprod/ndscm/configloader/loader.go
@@ -3,6 +3,7 @@ package configloader
 import (
 	"os"
 	"path/filepath"
+	"regexp"
 
 	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
 	"github.com/ndscm/theseed/seed/infra/log/go/seedlog"
@@ -36,4 +37,36 @@ func LoadDirConfigs(worktree string, scmFilePaths []string) (map[string]*DirConf
 		result[scmFilePath] = dirConfig
 	}
 	return result, nil
+}
+
+var upstreamNameRegex = regexp.MustCompile(`^[0-9A-Za-z]([0-9A-Za-z-]*[0-9A-Za-z])?$`)
+
+func LoadRepoConfig(worktreePath string) (*RepoConfig, error) {
+	repoConfigPath := filepath.Join(worktreePath, "REPO.ndscm.ts")
+	seedlog.Debugf("Loading repo config: %s", repoConfigPath)
+	raw, err := os.ReadFile(repoConfigPath)
+	if err != nil {
+		return nil, seederr.Wrap(err)
+	}
+	repoConfig := &RepoConfig{}
+	err = tson.Unmarshal(raw, repoConfig)
+	if err != nil {
+		return nil, seederr.Wrap(err)
+	}
+	hasRebase := false
+	for upstreamName, upstreamConfig := range repoConfig.Upstream {
+		if !upstreamNameRegex.MatchString(upstreamName) {
+			return nil, seederr.WrapErrorf("invalid upstream name: %v", upstreamName)
+		}
+		if upstreamConfig.Converge == "rebase" {
+			if hasRebase {
+				seedlog.Warnf("Multiple rebase upstreams found. It's recommended to have only one rebase upstream")
+			}
+			if upstreamName != "default" {
+				seedlog.Warnf("It's recommended to set the upstream name to `default` for the rebase upstream")
+			}
+			hasRebase = true
+		}
+	}
+	return repoConfig, nil
 }
