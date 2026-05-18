18e6f7c1d5157776f79d7ec20ce5ce8949b2dfe3
Nagi <nagi@ndscm.com>
2026-05-18 00:44:57 -0700

    seed: amadeus: update ndscm dir configs for bazel
    Change-uuid: de3903a9-ab15-480a-9a59-aaef6cfd3571
    Side-effect-of-change-uuid: 21638d78-da6a-4fb4-be6d-928c1530b818
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/amadeus/wake/proto/DIR.ndscm.ts
+++ seed/newtype/amadeus/wake/proto/DIR.ndscm.ts
@@ -9,8 +9,10 @@ export default {
         "wakepbconnect",
       ],
       watch: "\\.proto$",
-      needBazelBuild: ":bootstrap",
-      run: "{{BAZEL_RUN}}",
+      bazel: {
+        build: ":bootstrap",
+        run: "{{BAZEL_RUN}}",
+      },
     },
   },
 } satisfies DirConfig
