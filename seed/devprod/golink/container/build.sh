9e03f86d13f32adb653ecee16d94e81037487259
Nagi <nagi@ndscm.com>
2026-05-21 09:44:50 -0700

    seed: golink: support multiple container engine to build container
    Change-uuid: c9356292-b17b-4cad-ae7e-547b9496be6f
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/golink/container/build.sh
+++ seed/devprod/golink/container/build.sh
@@ -3,10 +3,19 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 
-container_cli=${CONTAINER_CLI:-"docker"}
+container_engine=${CONTAINER_ENGINE:-"podman"}
+./seed/devprod/container/ubuntu/build.sh
 
 bazel build //seed/devprod/golink/server:golink-server_tar_gz
 cp -f ./bazel-bin/seed/devprod/golink/server/golink-server.tar.gz ./seed/devprod/golink/container/golink-server.tar.gz
 
 cd ./seed/devprod/golink/container/
-"${container_cli}" build -t ghcr.io/ndscm/seed-devprod-golink-container:latest .
+
+build_compat=()
+if [[ "${container_engine}" == "docker" ]]; then
+  build_compat+=("-f" "Containerfile")
+elif [[ "${container_engine}" == "podman" ]]; then
+  build_compat+=("--userns" "auto:size=65536")
+fi
+
+"${container_engine}" build "${build_compat[@]}" -t ghcr.io/ndscm/seed-devprod-golink-container:latest .
