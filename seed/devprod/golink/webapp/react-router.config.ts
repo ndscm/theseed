1af52279d1e1cf6bea938f1b88a940fe74b55988
Nagi <nagi@ndscm.com>
2026-05-21 03:59:55 -0700

    seed: golink: webapp: use static i18n build
    Change-uuid: 399ebbf7-4ad4-4f12-98fb-426f6c72f45d
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/golink/webapp/react-router.config.ts
+++ seed/devprod/golink/webapp/react-router.config.ts
@@ -1,7 +1,11 @@
 import { type Config } from "@react-router/dev/config"
 
+const DEFAULT_LANGUAGE = process.env.DEFAULT_LANGUAGE || "en"
+const BUILD_LANGUAGE = process.env.BUILD_LANGUAGE || DEFAULT_LANGUAGE
+
 const config: Config = {
-  buildDirectory: "dist",
+  buildDirectory:
+    "dist" + (BUILD_LANGUAGE == DEFAULT_LANGUAGE ? "" : `/${BUILD_LANGUAGE}`),
   ssr: false,
   async prerender() {
     return ["/"]
--- seed/devprod/golink/webapp/vite.config.ts
+++ seed/devprod/golink/webapp/vite.config.ts
@@ -2,8 +2,15 @@ import { reactRouter } from "@react-router/dev/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
+const DEFAULT_LANGUAGE = process.env.DEFAULT_LANGUAGE || "en"
+const BUILD_LANGUAGE = process.env.BUILD_LANGUAGE || DEFAULT_LANGUAGE
+
 export default defineConfig({
+  base: BUILD_LANGUAGE == DEFAULT_LANGUAGE ? "/" : `/${BUILD_LANGUAGE}/`,
   plugins: [reactRouter(), tsconfigPaths()],
+  define: {
+    BUILD_LANGUAGE: JSON.stringify(BUILD_LANGUAGE),
+  },
   build: {
     assetsDir: ".assets",
   },
