95c836763a1528679cb4e94025c0b2c2a32dcbd3
Nagi <nagi@ndscm.com>
2026-05-21 21:22:26 -0700

    seed: stuff: webapp: build es locales
    Change-uuid: 6f841a35-a683-4b39-b23b-b30671ce101a
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/office/stuff/webapp/i18next.config.ts
+++ seed/office/stuff/webapp/i18next.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "i18next-cli"
 
 export default defineConfig({
-  locales: ["en"],
+  locales: [
+    "en", // en must be defined as the first language
+    "es",
+  ],
   extract: {
     input: "**/*.{ts,tsx}",
     output: "locales/{{language}}/{{namespace}}.json",
