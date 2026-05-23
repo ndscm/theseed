f796a19dc8d4f8f3aeabb9efdc7be1bab0906182
Nagi <nagi@ndscm.com>
2026-05-22 23:16:53 -0700

    seed: stuff: use flatten login service context
    Change-uuid: b019daf3-1aa6-4977-a55c-b88f74b4dabd
    Side-effect-of-change-uuid: 4d4b0ab5-bbb6-457f-8b7f-e3a3e061a662
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/office/stuff/webapp/app/routes/layout.tsx
+++ seed/office/stuff/webapp/app/routes/layout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router"
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
 
-import { LoginServiceProvider } from "@//seed/cloud/login/client/tsx/login-service-context"
+import { LoginServiceProvider } from "@//seed/cloud/login/client/tsx/LoginServiceContext"
 import Gotcha from "@//seed/devprod/gotcha/tsx"
 import { StuffServiceProvider } from "@//seed/office/stuff/client/tsx/stuff-service-context"
 
