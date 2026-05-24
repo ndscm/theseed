68513f06a1688a43c9251a3a7ab208272fe11fae
Nagi <nagi@ndscm.com>
2026-05-24 08:39:22 -0700

    seed: sfe: support multiple container engine build
    Change-uuid: 34d5e190-7cbe-4b82-9eb6-c5070cde603d
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/cloud/sfe/container/build.sh
+++ seed/cloud/sfe/container/build.sh
@@ -3,10 +3,20 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 
-container_cli=${CONTAINER_CLI:-"docker"}
+container_engine=${CONTAINER_ENGINE:-"podman"}
+
+./seed/devprod/container/ubuntu/build.sh
 
 bazel build //seed/cloud/sfe/server
 cp -f ./bazel-bin/seed/cloud/sfe/server/sfe-server_/sfe-server ./seed/cloud/sfe/container/sfe-server
 
 cd ./seed/cloud/sfe/container/
-"${container_cli}" build -t ghcr.io/ndscm/seed-cloud-sfe-container:latest .
+
+build_compat=()
+if [[ "${container_engine}" == "docker" ]]; then
+  build_compat+=("-f" "Containerfile")
+elif [[ "${container_engine}" == "podman" ]]; then
+  build_compat+=("--userns" "auto:size=65536")
+fi
+
+"${container_engine}" build "${build_compat[@]}" -t ghcr.io/ndscm/seed-cloud-sfe-container:latest .
--- seed/cloud/sfe/container/start.sh
+++ seed/cloud/sfe/container/start.sh
@@ -3,11 +3,12 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-container_cli=${CONTAINER_CLI:-"docker"}
+container_engine=${CONTAINER_ENGINE:-"podman"}
 
 ./build.sh
 
-"${container_cli}" run --name "sfe" \
-  --interactive --rm --tty \
+"${container_engine}" run --name "sfe" --rm --interactive --tty \
   --network "host" \
-  ghcr.io/ndscm/seed-cloud-sfe-container:latest
+  ghcr.io/ndscm/seed-cloud-sfe-container:latest \
+  --http_port 8080 \
+  --verbose
