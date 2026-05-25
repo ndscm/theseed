277baa591f87216692cb4939453bd962a346a108
Nagi <nagi@ndscm.com>
2026-05-25 13:39:10 -0700

    seed: amadeus: support multiple container engines
    Change-uuid: a06f6360-9567-49d1-b37e-f39c058b17a9
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/amadeus/container/build.sh
+++ seed/newtype/amadeus/container/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -eux
 set -o pipefail
-cd $(dirname "${BASH_SOURCE[0]}")/../../../..
+cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
+
+container_engine=${CONTAINER_ENGINE:-"podman"}
 
 bazel build //seed/devprod/ndscm/cli
 cp -f ./bazel-bin/seed/devprod/ndscm/cli/ndscm_/ndscm ./seed/newtype/amadeus/container/ndscm
@@ -10,4 +12,12 @@ bazel build //seed/newtype/amadeus/server
 cp -f ./bazel-bin/seed/newtype/amadeus/server/amadeus-server_/amadeus-server ./seed/newtype/amadeus/container/amadeus-server
 
 cd ./seed/newtype/amadeus/container/
-"${CONTAINER_CLI}" build -t ghcr.io/ndscm/seed-newtype-amadeus-container:latest .
+
+build_compat=()
+if [[ "${container_engine}" == "docker" ]]; then
+  build_compat+=("-f" "Containerfile")
+elif [[ "${container_engine}" == "podman" ]]; then
+  build_compat+=("--userns" "auto:size=65536")
+fi
+
+"${container_engine}" build "${build_compat[@]}" -t ghcr.io/ndscm/seed-newtype-amadeus-container:latest .
