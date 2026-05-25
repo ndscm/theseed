f85b57735f32b816379fcc74c8b7ed42d0aacfdb
Nagi <nagi@ndscm.com>
2026-05-25 13:43:55 -0700

    seed: hooin: onpremise: add send shortcut script
    Change-uuid: c3b2a7d8-0c4b-4a2d-9ba1-7a98bc739c8d
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/newtype/hooin/deploy/onpremise/send.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+person=${1:-"christina"}
+topic=${2:-"review"}
+text=${3:-"I need to review the pull requests."}
+
+bazel run //seed/newtype/hooin/cmd/send -- \
+  --hooin_dictate_service_server http://newtype.ndscm.com:4664 \
+  --login_openid_discovery_url https://account.ndscm.com/realms/ndscm/.well-known/openid-configuration \
+  --login_tier prod \
+  --person "${person}" \
+  --topic "${topic}" \
+  --stream \
+  --text "${text}" \
+  --verbose
