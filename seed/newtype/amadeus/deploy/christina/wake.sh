25f682e7d52e4e2058793b91f5b8104406fecba9
Nagi <nagi@ndscm.com>
2026-05-25 13:43:40 -0700

    seed: amadeus: christina: add wake shortcut script
    Change-uuid: 0378f99d-1db2-49a1-b77c-4823de2342e8
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/newtype/amadeus/deploy/christina/wake.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+token=${1:-"christina:unsecure"}
+
+bazel run //seed/newtype/amadeus/cmd/wake -- \
+  --amadeus_service_server http://newtype.ndscm.com:2447 \
+  --login_openid_discovery_url https://account.ndscm.com/realms/ndscm/.well-known/openid-configuration \
+  --login_tier prod \
+  --hooin_direct_server http://newtype.ndscm.com:4664 \
+  --token "${token}" \
+  --verbose
