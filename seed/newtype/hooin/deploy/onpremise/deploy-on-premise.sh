12fd41411d37dbdd6927d6af554666bf55e81662
Nagi <nagi@ndscm.com>
2026-05-25 13:39:50 -0700

    seed: hooin: onpremise: deploy with docker engine
    Change-uuid: 3886d8fa-3417-4835-9cca-9ab7e6e0ce85
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/hooin/deploy/onpremise/deploy-on-premise.sh
+++ seed/newtype/hooin/deploy/onpremise/deploy-on-premise.sh
@@ -6,16 +6,16 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../../../../.."
 server=${1:-"steins.ndscm.com"}
 mount_etc_steins=${2:-"/mnt/data/steins/etc/steins"}
 
-container_cli=${CONTAINER_CLI:-"docker"}
-
+export CONTAINER_ENGINE="docker"
 ./seed/newtype/hooin/container/build.sh
-"${container_cli}" save ghcr.io/ndscm/seed-newtype-hooin-container:latest | ssh "${server}" "${container_cli} load"
+
+docker save ghcr.io/ndscm/seed-newtype-hooin-container:latest | ssh "${server}" "docker load"
 
 printf "Use this command to access the container:\n"
-printf "    \x1b[1;33m${container_cli} --host \"ssh://${server}\" exec --interactive --tty --user hooin hooin zsh\x1b[0m\n"
+printf "    \x1b[1;33mdocker --host \"ssh://${server}\" exec --interactive --tty --user hooin hooin zsh\x1b[0m\n"
 
-"${container_cli}" --host "ssh://${server}" rm -f hooin || true
-"${container_cli}" --host "ssh://${server}" run --name hooin --interactive --tty \
+docker --host "ssh://${server}" rm -f hooin || true
+docker --host "ssh://${server}" run --name hooin --interactive --tty \
   --network=host \
   --volume "${mount_etc_steins}:/etc/steins" \
   ghcr.io/ndscm/seed-newtype-hooin-container:latest
