f95ef556e68431018cf974772acc860202c292b3
Nagi <nagi@ndscm.com>
2026-05-25 22:42:49 -0700

    seed: sfe: certificate: deploy to gcloud
    Change-uuid: e1eb3aed-7acb-4f0d-8aac-ec49729a883f
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/cloud/sfe/certificate/deploy/gcloud/deploy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")/../../../../../.."
+
+container_engine=${CONTAINER_ENGINE:-"podman"}
+
+project="ndscm-prod"
+region="us-west1"
+service="seed-cloud-sfe-certificate-prod"
+image_package="us-docker.pkg.dev/ndscm-prod/container-us/seed-cloud-sfe-certificate-deploy-gcloud"
+
+export CONTAINER_ENGINE="${container_engine}"
+./seed/cloud/sfe/certificate/container/build.sh
+
+cd ./seed/cloud/sfe/certificate/deploy/gcloud/
+
+build_compat=()
+if [[ "${container_engine}" == "docker" ]]; then
+  build_compat+=("-f" "Containerfile")
+elif [[ "${container_engine}" == "podman" ]]; then
+  build_compat+=("--userns" "auto:size=65536")
+fi
+
+"${container_engine}" build "${build_compat[@]}" -t "${image_package}:prod" .
+
+"${container_engine}" push "${image_package}:prod"
+
+image_digest=$(crane digest "${image_package}:prod")
+gcloud run services update "${service}" \
+  --project="${project}" \
+  --region="${region}" \
+  --image="${image_package}@${image_digest}" \
+  --max-instances=1 \
+  --max=1
