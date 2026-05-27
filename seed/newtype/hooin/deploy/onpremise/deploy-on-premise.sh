d7f2a9976f83b8febae696ad9a89dc0827b45727
Nagi <nagi@ndscm.com>
2026-05-26 20:36:01 -0700

    seed: hooin: propagate container engine to base image
    Change-uuid: 927a27ab-d335-4366-9cfb-71247a3e3416
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/hooin/deploy/onpremise/deploy-on-premise.sh
+++ seed/newtype/hooin/deploy/onpremise/deploy-on-premise.sh
@@ -6,7 +6,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../../../../.."
 server=${1:-"steins.ndscm.com"}
 mount_etc_steins=${2:-"/mnt/data/steins/etc/steins"}
 
-export CONTAINER_ENGINE="docker"
+container_engine=${CONTAINER_ENGINE:-"docker"}
+if [[ "${container_engine}" != "docker" ]]; then
+  printf 'Only docker is supported for now\n'
+  exit 1
+fi
+
+export CONTAINER_ENGINE="${container_engine}"
 ./seed/newtype/hooin/container/build.sh
 
 docker save ghcr.io/ndscm/seed-newtype-hooin-container:latest | ssh "${server}" "docker load"
