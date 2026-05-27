67298539e772d7401e8022d9c35582cba53a4fe1
Nagi <nagi@ndscm.com>
2026-05-27 03:56:29 -0700

    seed: test: simplify with ndscm build and test
    Change-uuid: 48ec89f2-ea21-4c48-998e-3aceec66530e
    Signed-off-by: Nagi <nagi@ndscm.com>

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 set -o pipefail
-cd $(dirname "${BASH_SOURCE[0]}")
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # Test all commits:
 # $ git rebase --interactive --exec "./test.sh"
@@ -9,8 +9,4 @@ cd $(dirname "${BASH_SOURCE[0]}")
 # Test commits since a specific commit:
 # $ git rebase --interactive --exec "./test.sh" <commit-hash-or-tag>
 
-bazel build //...
-
-bazel test //...
-
-bazel run @pnpm//:pnpm -- --dir $PWD/seed/devprod/ts/tool/verify-pnpm-lock run verify-pnpm-lock
+ndscm test
