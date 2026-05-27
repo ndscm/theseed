61069da52da53aa567f3a6c3611f2b115687d220
Nagi <nagi@ndscm.com>
2026-05-27 05:29:59 -0700

    seed: keycloak: gcloud: deploy with both container engine
    Change-uuid: c06ef718-532b-419a-b848-d78fdf6871a3
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/cloud/keycloak/deploy/gcloud/deploy.sh
+++ seed/cloud/keycloak/deploy/gcloud/deploy.sh
@@ -3,23 +3,32 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../../.."
 
-container_cli=${CONTAINER_CLI:-"docker"}
+container_engine=${CONTAINER_ENGINE:-"podman"}
 
 project="ndscm-prod"
 region="us-west1"
 service="seed-cloud-keycloak-prod"
 image_package="us-docker.pkg.dev/ndscm-prod/container-us/seed-cloud-keycloak-deploy-gcloud"
 
+export CONTAINER_ENGINE="${container_engine}"
 ./seed/cloud/keycloak/container/build.sh
 
 cd ./seed/cloud/keycloak/deploy/gcloud/
-"${container_cli}" build -t "${image_package}:prod" .
 
-"${container_cli}" push "${image_package}:prod"
+build_compat=()
+if [[ "${container_engine}" == "docker" ]]; then
+  build_compat+=("-f" "Containerfile")
+elif [[ "${container_engine}" == "podman" ]]; then
+  build_compat+=("--userns" "auto:size=65536")
+fi
 
-image_digest="$("${container_cli}" inspect --format='{{index .RepoDigests 0}}' "${image_package}:prod")"
+"${container_engine}" build "${build_compat[@]}" -t "${image_package}:prod" .
+
+"${container_engine}" push "${image_package}:prod"
+
+image_digest=$(crane digest "${image_package}:prod")
 gcloud run services update "${service}" \
   --project="${project}" \
   --region="${region}" \
-  --image="${image_digest}" \
+  --image="${image_package}@${image_digest}" \
   --startup-probe="httpGet.port=9000,httpGet.path=/health/ready,initialDelaySeconds=1,periodSeconds=1,timeoutSeconds=1,failureThreshold=120"
