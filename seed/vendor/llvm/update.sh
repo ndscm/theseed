521156e098667461d4946eb59fd1b4f3a4303052
Nagi <nagi@ndscm.com>
2026-05-27 01:23:14 -0700

    seed: vendor: llvm: add dotslash entry for clang, clangd and clang-format
    Change-uuid: ff013a50-7229-4c56-ae50-9dd4c4edddda
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/vendor/llvm/update.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
+
+tag="21.1.8"
+
+bazel run //seed/devprod/dotslash/update -- \
+  --skeleton "$(pwd)/seed/vendor/llvm/clang-format.dotslash.json" \
+  --tag="${tag}" \
+  >./seed/vendor/llvm/bin/clang-format
+chmod +x ./seed/vendor/llvm/bin/clang-format
+
+bazel run //seed/devprod/dotslash/update -- \
+  --skeleton "$(pwd)/seed/vendor/llvm/clang.dotslash.json" \
+  --tag="${tag}" \
+  >./seed/vendor/llvm/bin/clang
+chmod +x ./seed/vendor/llvm/bin/clang
+
+bazel run //seed/devprod/dotslash/update -- \
+  --skeleton "$(pwd)/seed/vendor/llvm/clangd.dotslash.json" \
+  --tag="${tag}" \
+  >./seed/vendor/llvm/bin/clangd
+chmod +x ./seed/vendor/llvm/bin/clangd
