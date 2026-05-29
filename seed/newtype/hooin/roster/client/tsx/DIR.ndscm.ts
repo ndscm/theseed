db1983991cfc5050aa6a94808d61c0d94362b941
Nagi <nagi@ndscm.com>
2026-05-29 01:27:43 -0700

    seed: hooin: roster: add tsx service context
    Change-uuid: 13c48133-3692-45d6-b022-9eea2cf3e2ba
    Break: lock
    Migrate: update lock
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/newtype/hooin/roster/client/tsx/DIR.ndscm.ts
@@ -0,0 +1,18 @@
+import { type DirConfig } from "@//seed/devprod/ndscm/config/DIR"
+
+export default {
+  bootstrap: {
+    dist: {
+      target: "dist",
+      watchRepo: [
+        "node_modules",
+        "seed/newtype/hooin/roster/proto/roster_pb.js",
+      ],
+      watch: "^src/",
+      bazel: {
+        build: "@pnpm//:pnpm",
+        run: 'BAZEL_BINDIR="$(dirname "{{BAZEL_EXECUTABLE}}")" {{BAZEL_RUN}} --dir "$(pwd)" build',
+      },
+    },
+  },
+} satisfies DirConfig
