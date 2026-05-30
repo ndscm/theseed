ad3e598a8fd193bf59bae2a4c1ad4b3c281bbbc8
Nagi <nagi@ndscm.com>
2026-05-30 02:00:41 -0700

    seed: golink: webapp: use vite dev login and headless login button
    Change-uuid: 41ed4f34-4de4-49f7-8637-3a7a46b980e1
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/golink/webapp/vite.config.ts
+++ seed/devprod/golink/webapp/vite.config.ts
@@ -2,12 +2,19 @@ import { reactRouter } from "@react-router/dev/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
+import unsafeDevLogin from "../../../cloud/login/ts/vite-plugin-unsafe-dev-login"
+
 const DEFAULT_LANGUAGE = process.env.DEFAULT_LANGUAGE || "en"
 const BUILD_LANGUAGE = process.env.BUILD_LANGUAGE || DEFAULT_LANGUAGE
 
 export default defineConfig({
   base: BUILD_LANGUAGE == DEFAULT_LANGUAGE ? "/" : `/${BUILD_LANGUAGE}/`,
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    //sort
+    reactRouter(),
+    tsconfigPaths(),
+    unsafeDevLogin({ clientId: "webapp-dev" }),
+  ],
   define: {
     BUILD_LANGUAGE: JSON.stringify(BUILD_LANGUAGE),
   },
@@ -16,6 +23,9 @@ export default defineConfig({
   },
   server: {
     proxy: {
+      "/seed.cloud.login.proto.LoginService/": {
+        target: process.env.LOGIN_SERVICE_SERVER || "",
+      },
       "/seed.devprod.golink.proto.GolinkService/": {
         target: process.env.GOLINK_SERVICE_SERVER || "",
       },
