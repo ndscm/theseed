f0a2686f30e86c4d4cbbaa19c854ec925df10c35
Nagi <nagi@ndscm.com>
2026-05-31 12:20:38 -0700

    seed: auth: go: fix typo for response types supported
    Change-uuid: 3b567b49-ad18-4222-a28e-7a1a571e8b3c
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/infra/auth/go/openid/configuration.go
+++ seed/infra/auth/go/openid/configuration.go
@@ -8,16 +8,16 @@ import (
 
 type OpenidConfiguration struct {
 	// See: https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderMetadata
-	Issuer                  string   `json:"issuer"`
-	AuthorizationEndpoint   string   `json:"authorization_endpoint"`
-	TokenEndpoint           string   `json:"token_endpoint"`
-	UserinfoEndpoint        string   `json:"userinfo_endpoint"`
-	JwksUri                 string   `json:"jwks_uri"`
-	ScopesSupported         []string `json:"scopes_supported"`
-	ResponsesTypesSupported []string `json:"responses_types_supported"`
-	GrantTypesSupported     []string `json:"grant_types_supported"`
-	SubjectTypesSupported   []string `json:"subject_types_supported"`
-	ClaimsSupported         []string `json:"claims_supported"`
+	Issuer                 string   `json:"issuer"`
+	AuthorizationEndpoint  string   `json:"authorization_endpoint"`
+	TokenEndpoint          string   `json:"token_endpoint"`
+	UserinfoEndpoint       string   `json:"userinfo_endpoint"`
+	JwksUri                string   `json:"jwks_uri"`
+	ScopesSupported        []string `json:"scopes_supported"`
+	ResponseTypesSupported []string `json:"response_types_supported"`
+	GrantTypesSupported    []string `json:"grant_types_supported"`
+	SubjectTypesSupported  []string `json:"subject_types_supported"`
+	ClaimsSupported        []string `json:"claims_supported"`
 
 	// See: https://datatracker.ietf.org/doc/html/rfc8628#section-7.4
 	DeviceAuthorizationEndpoint string `json:"device_authorization_endpoint,omitempty"`
