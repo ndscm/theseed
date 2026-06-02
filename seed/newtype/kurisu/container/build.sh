53f295d13acb9a33be2b8e651e38a19e787aed64
Nagi <nagi@ndscm.com>
2026-06-02 00:08:06 -0700

    seed: kurisu: build kurisu container
    Change-uuid: 2f38f847-5217-415c-ae6f-255879ef6d8d
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/newtype/kurisu/container/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
+
+container_engine=${CONTAINER_ENGINE:-"podman"}
+
+export CONTAINER_ENGINE="${container_engine}"
+./seed/devprod/container/ubuntu/build.sh
+
+bazel build //seed/newtype/kurisu/server:kurisu-server_tar_gz
+cp -f ./bazel-bin/seed/newtype/kurisu/server/kurisu-server.tar.gz ./seed/newtype/kurisu/container/kurisu-server.tar.gz
+
+cd ./seed/newtype/kurisu/container/
+
+build_compat=()
+if [[ "${container_engine}" == "docker" ]]; then
+  build_compat+=("-f" "Containerfile")
+elif [[ "${container_engine}" == "podman" ]]; then
+  build_compat+=("--userns" "auto:size=65536")
+fi
+
+"${container_engine}" build "${build_compat[@]}" -t ghcr.io/ndscm/seed-newtype-kurisu-container:latest .
