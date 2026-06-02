a75acf3e3808abd0afa2799534c01c8be3e6d550
Nagi <nagi@ndscm.com>
2026-06-02 00:08:06 -0700

    seed: kurisu: build kurisu container
    Change-uuid: 2f38f847-5217-415c-ae6f-255879ef6d8d
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/newtype/kurisu/container/start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+container_engine=${CONTAINER_ENGINE:-"podman"}
+
+export CONTAINER_ENGINE="${container_engine}"
+./build.sh
+
+"${container_engine}" run --name "kurisu" --rm --interactive --tty \
+  --network "host" \
+  ghcr.io/ndscm/seed-newtype-kurisu-container:latest
