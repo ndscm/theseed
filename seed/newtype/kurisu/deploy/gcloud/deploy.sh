e62730e92f4860c80ef979ff1fbaeee0d2fa36a3
Nagi <nagi@ndscm.com>
2026-06-07 21:54:18 -0700

    seed: kurisu: gcloud: deploy kurisu on gcloud
    Change-uuid: 1b716cc3-41f4-44a7-99c6-5d32c69df324
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/newtype/kurisu/deploy/gcloud/deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")/../../../../.."
+
+container_engine=${CONTAINER_ENGINE:-"podman"}
+
+project="ndscm-prod"
+region="us-west1"
+service="seed-newtype-kurisu-prod"
+image_package="us-docker.pkg.dev/ndscm-prod/container-us/seed-newtype-kurisu-deploy-gcloud"
+
+export CONTAINER_ENGINE="${container_engine}"
+./seed/newtype/kurisu/container/build.sh
+
+cd ./seed/newtype/kurisu/deploy/gcloud/
+
+build_compat=()
+if [[ "${container_engine}" == "docker" ]]; then
+  build_compat+=("-f" "Containerfile")
+elif [[ "${container_engine}" == "podman" ]]; then
+  build_compat+=("--userns" "auto:size=65536")
+fi
+
+"${container_engine}" build "${build_compat[@]}" -t "${image_package}:prod" .
+
+"${container_engine}" push "${image_package}:prod"
+
+image_digest=$(crane digest "${image_package}:prod")
+gcloud run services update "${service}" --project="${project}" --region="${region}" --image="${image_package}@${image_digest}"
