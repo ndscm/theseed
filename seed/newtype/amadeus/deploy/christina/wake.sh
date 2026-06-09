da8e6495abc7d162764dc37eae120a126cb822e2
Nagi <nagi@ndscm.com>
2026-06-09 02:02:11 -0700

    seed: amadeus: onpremise: move christina deploy script to onpremise
    Change-uuid: a69191c5-86a2-4984-b096-07eaff959a33
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/amadeus/deploy/christina/wake.sh
+++ seed/newtype/amadeus/deploy/christina/wake.sh
@@ -3,11 +3,8 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-token=${1:-"christina:unsecure"}
-
 bazel run //seed/newtype/amadeus/cmd/wake -- \
   --amadeus_service_server http://newtype.ndscm.com:2447 \
   --login_tier prod \
   --hooin_direct_server http://newtype.ndscm.com:4664 \
-  --token "${token}" \
   --verbose
