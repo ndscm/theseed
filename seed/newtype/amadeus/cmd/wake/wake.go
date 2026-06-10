19181d36a863429db1291ad507c2e149e2d04ebe
Nagi <nagi@ndscm.com>
2026-06-09 23:46:17 -0700

    seed: amadeus: wake: remove hooin direct server flag
    Change-uuid: 74de5991-b5d8-4220-b74b-96633379816e
    Side-effect-of-change-uuid: 84fed134-c89e-45d5-9274-41f9860e9367
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/amadeus/cmd/wake/wake.go
+++ seed/newtype/amadeus/cmd/wake/wake.go
@@ -6,13 +6,10 @@ import (
 
 	"github.com/ndscm/theseed/seed/cloud/login/go/devicelogin"
 	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
-	"github.com/ndscm/theseed/seed/infra/flag/go/seedflag"
 	"github.com/ndscm/theseed/seed/infra/init/go/seedinit"
 	"github.com/ndscm/theseed/seed/infra/log/go/seedlog"
 	"github.com/ndscm/theseed/seed/newtype/amadeus/wake/client/go/wakeclient"
 )
-
-var flagHooinDirectServer = seedflag.DefineString("hooin_direct_server", "", "Hooin instance address")
 
 func run() error {
 	_, err := seedinit.Initialize(
@@ -27,7 +24,7 @@ func run() error {
 		return seederr.Wrap(err)
 	}
 	client := wakeclient.NewAmadeusWakeClient("")
-	err = client.Wake(ctx, flagHooinDirectServer.Get())
+	err = client.Wake(ctx)
 	if err != nil {
 		return seederr.Wrap(err)
 	}
