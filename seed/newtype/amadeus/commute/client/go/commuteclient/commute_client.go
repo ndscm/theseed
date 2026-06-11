256ae3c310871ed938d6cf5cf37a04f651906469
Nagi <nagi@ndscm.com>
2026-06-10 19:15:37 -0700

    seed: amadeus: commute: add amadeus commute client
    Change-uuid: 502d6f9e-a41b-4411-ab68-a9885427210c
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/newtype/amadeus/commute/client/go/commuteclient/commute_client.go
@@ -0,0 +1,66 @@
+package commuteclient
+
+import (
+	"context"
+	"net/http"
+
+	"connectrpc.com/connect"
+	"github.com/ndscm/theseed/seed/infra/flag/go/seedflag"
+	"github.com/ndscm/theseed/seed/infra/grpc/go/grpclog"
+	"github.com/ndscm/theseed/seed/infra/http/go/seedbearer"
+	"github.com/ndscm/theseed/seed/newtype/amadeus/commute/proto/commutepb"
+	"github.com/ndscm/theseed/seed/newtype/amadeus/commute/proto/commutepbconnect"
+	"github.com/ndscm/theseed/seed/newtype/gajetto/proto/brainpb"
+)
+
+var flagAmadeusCommuteServiceServer = seedflag.DefineString("amadeus_commute_service_server", "http://127.0.0.1:2623", "Amadeus commute service server address")
+
+type AmadeusCommuteClient struct {
+	client commutepbconnect.AmadeusCommuteServiceClient
+}
+
+func (c *AmadeusCommuteClient) SendBrainInput(
+	ctx context.Context,
+	input *brainpb.BrainInput,
+) error {
+	_, err := c.client.SendBrainInput(ctx, connect.NewRequest(
+		&commutepb.SendBrainInputRequest{
+			BrainInput: input,
+		}))
+	return err
+}
+
+type amadeusCommuteClientOptions struct {
+	httpClient *http.Client
+	server     string
+}
+
+type AmadeusCommuteClientOption func(*amadeusCommuteClientOptions)
+
+func WithHttpClient(httpClient *http.Client) AmadeusCommuteClientOption {
+	return func(opts *amadeusCommuteClientOptions) {
+		opts.httpClient = httpClient
+	}
+}
+
+func WithServer(server string) AmadeusCommuteClientOption {
+	return func(opts *amadeusCommuteClientOptions) {
+		opts.server = server
+	}
+}
+
+func NewAmadeusCommuteClient(opts ...AmadeusCommuteClientOption) *AmadeusCommuteClient {
+	o := &amadeusCommuteClientOptions{
+		httpClient: http.DefaultClient,
+		server:     flagAmadeusCommuteServiceServer.Get(),
+	}
+	for _, opt := range opts {
+		opt(o)
+	}
+	client := commutepbconnect.NewAmadeusCommuteServiceClient(
+		seedbearer.InterceptBearerTransport(o.httpClient),
+		o.server,
+		connect.WithInterceptors(grpclog.NewLogInterceptor()),
+	)
+	return &AmadeusCommuteClient{client}
+}
