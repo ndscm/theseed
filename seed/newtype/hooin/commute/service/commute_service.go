14c56f7efd7d258934fb24a24204722861510f15
Nagi <nagi@ndscm.com>
2026-06-10 20:38:24 -0700

    seed: hooin: commute: remove commute rpc handler
    Change-uuid: 2ab6a157-0c86-4efd-b1c6-c861c1a91f4e
    Side-effect-of-change-uuid: f814e43d-0ea4-41d5-8cb5-69d90447b64e
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/hooin/commute/service/commute_service.go
+++ seed/newtype/hooin/commute/service/commute_service.go
@@ -5,7 +5,6 @@ import (
 
 	"connectrpc.com/connect"
 	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
-	"github.com/ndscm/theseed/seed/newtype/gajetto/proto/brainpb"
 	"github.com/ndscm/theseed/seed/newtype/hooin/commute/proto/commutepb"
 	"github.com/ndscm/theseed/seed/newtype/hooin/onsite"
 	"google.golang.org/grpc/codes"
@@ -14,14 +13,6 @@ import (
 
 type HooinCommuteService struct {
 	office *onsite.Office
-}
-
-func (svc *HooinCommuteService) Commute(
-	ctx context.Context,
-	req *connect.Request[commutepb.CommuteRequest],
-	stream *connect.ServerStream[brainpb.BrainInput],
-) error {
-	return seederr.CodeErrorf(codes.Unimplemented, "Commute is not implemented")
 }
 
 func (svc *HooinCommuteService) ReportBrainStep(
