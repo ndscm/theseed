384c40288aa7cfa310b5f39a706b848fa30fe8ea
Nagi <nagi@ndscm.com>
2026-06-15 08:40:46 -0700

    seed: amadeus: switch back to root entrypoint
    Change-uuid: 4c685223-f6bc-4b2c-8ee3-57bfd09c995b
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/amadeus/container/build.sh
+++ seed/newtype/amadeus/container/build.sh
@@ -8,8 +8,19 @@ container_engine=${CONTAINER_ENGINE:-"podman"}
 export CONTAINER_ENGINE="${container_engine}"
 ./seed/devprod/container/ubuntu/build.sh
 
+mkdir -p ./seed/newtype/amadeus/container/bin/
+
 bazel build //seed/devprod/ndscm/cli
-cp -f ./bazel-bin/seed/devprod/ndscm/cli/ndscm_/ndscm ./seed/newtype/amadeus/container/ndscm
+cp -f ./bazel-bin/seed/devprod/ndscm/cli/ndscm_/ndscm ./seed/newtype/amadeus/container/bin/ndscm
+
+cp -f ./seed/vendor/bazel/bin/bazel ./seed/newtype/amadeus/container/bin/bazel
+cp -f ./seed/vendor/bazel/bin/bazelisk ./seed/newtype/amadeus/container/bin/bazelisk
+cp -f ./seed/vendor/buck/bin/buck2 ./seed/newtype/amadeus/container/bin/buck2
+cp -f ./seed/vendor/buck/bin/reindeer ./seed/newtype/amadeus/container/bin/reindeer
+cp -f ./seed/vendor/claude/bin/claude ./seed/newtype/amadeus/container/bin/claude
+cp -f ./seed/vendor/crane/bin/crane ./seed/newtype/amadeus/container/bin/crane
+cp -f ./seed/vendor/lego/bin/lego ./seed/newtype/amadeus/container/bin/lego
+cp -f ./seed/vendor/llvm/bin/clang ./seed/newtype/amadeus/container/bin/clang
 
 bazel build //seed/newtype/amadeus/server
 cp -f ./bazel-bin/seed/newtype/amadeus/server/amadeus-server_/amadeus-server ./seed/newtype/amadeus/container/amadeus-server
