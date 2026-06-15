cda04aeb1f71a790b94c2824141de0741693997d
Nagi <nagi@ndscm.com>
2026-06-14 19:14:02 -0700

    seed: kurisu: update start script to connect to prod hooin for development
    Change-uuid: 723c7310-d25d-4f45-a9d3-92b91e66180e
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/kurisu/server/start.sh
+++ seed/newtype/kurisu/server/start.sh
@@ -8,9 +8,9 @@ stack=${1:-"local"}
 hooin_dictate_service_server="http://127.0.0.1:4664"
 hooin_roster_service_server="http://127.0.0.1:4664"
 
-if [[ "$stack" == "onpremise" ]]; then
-  hooin_dictate_service_server="http://steins.ndscm.biz:4664"
-  hooin_roster_service_server="http://steins.ndscm.biz:4664"
+if [[ "$stack" == "prod" ]]; then
+  hooin_dictate_service_server="https://hooin.ndscm.biz"
+  hooin_roster_service_server="https://hooin.ndscm.biz"
 fi
 
 bazel run //seed/newtype/kurisu/server -- \
