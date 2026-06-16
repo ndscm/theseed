a6a7a6d0a3a37d5a117104bd6ab26eb577c566ca
Nagi <nagi@ndscm.com>
2026-06-16 00:26:29 -0700

    seed: amadeus: reconnect after stream is closed
    Change-uuid: bd3926a3-a426-422e-8a32-985c61d0cec3
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/amadeus/onduty/step_reporter.go
+++ seed/newtype/amadeus/onduty/step_reporter.go
@@ -6,21 +6,25 @@ import (
 	"github.com/ndscm/theseed/seed/cloud/login/go/siliconlogin"
 	"github.com/ndscm/theseed/seed/infra/log/go/seedlog"
 	"github.com/ndscm/theseed/seed/newtype/gajetto/proto/brainpb"
-	"github.com/ndscm/theseed/seed/newtype/hooin/commute/client/go/commuteclient"
 )
 
 type StepReporter struct {
-	client *commuteclient.HooinCommuteClient
+	conscious *Conscious
 }
 
 func (r *StepReporter) HandleBrainStep(
 	ctx context.Context, topic string, step *brainpb.BrainStep,
 ) {
+	client := r.conscious.getCommuteClient()
+	if client == nil {
+		seedlog.Warnf("Dropping brain step for topic %q: no active connection", topic)
+		return
+	}
 	ctx, err := siliconlogin.SiliconLogin(ctx)
 	if err != nil {
 		seedlog.Warnf("Silicon login failed: %v", err)
 	}
-	err = r.client.ReportBrainStep(ctx, step)
+	err = client.ReportBrainStep(ctx, step)
 	if err != nil {
 		seedlog.Errorf("Report brain step failed: %v", err)
 	}
