85cdd72f6391af2ed50ccf6d143f541ccd50eb1e
Nagi <nagi@ndscm.com>
2026-06-17 04:07:22 -0700

    seed: setproxy: format setproxy script
    Change-uuid: 8d8682ab-f8b1-43dc-b1ff-8fce11a56d0c
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/setproxy.sh
+++ seed/devprod/setproxy.sh
@@ -12,13 +12,13 @@
 proxy=${1:-""}
 
 if [[ "$proxy" == "" ]]; then
-    unset HTTPS_PROXY
-    unset HTTP_PROXY
-    unset https_proxy
-    unset http_proxy
+  unset HTTPS_PROXY
+  unset HTTP_PROXY
+  unset https_proxy
+  unset http_proxy
 else
-    export HTTPS_PROXY=$proxy
-    export HTTP_PROXY=$proxy
-    export https_proxy=$proxy
-    export http_proxy=$proxy
+  export HTTPS_PROXY=$proxy
+  export HTTP_PROXY=$proxy
+  export https_proxy=$proxy
+  export http_proxy=$proxy
 fi
