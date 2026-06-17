0916476908edb0f33ceeaa1cc231fb43ec5994bd
Nagi <nagi@ndscm.com>
2026-06-17 09:29:08 -0700

    seed: smd: workstation: install dotslash
    Change-uuid: 8ff193f7-ac10-4567-b9aa-d1d44de7da38
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/smd/workstation/maintain-11-basic-packages.sh
+++ seed/devprod/smd/workstation/maintain-11-basic-packages.sh
@@ -6,6 +6,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 printf "\e[34mChecking basic packages...\e[0m\n"
 
 if [[ "${oslike}" == "debian" ]]; then
+  mkdir -p "${HOME}/.local/bin"
   if [[ "${run_sudo}" == "true" ]]; then
     sudo apt update
     sudo apt upgrade -y
@@ -14,12 +15,16 @@ if [[ "${oslike}" == "debian" ]]; then
     sudo apt install -y git
     sudo apt install -y netcat-openbsd
     sudo apt install -y ssh
+    sudo apt install -y tar
   else
     printf "\e[31mSkipping system package installation\e[0m\n"
   fi
+
+  curl -fsSL "https://github.com/facebook/dotslash/releases/latest/download/dotslash-ubuntu-22.04.$(uname -m).tar.gz" | tar fxz - -C "${HOME}/.local/bin"
 fi
 
 if [[ "${oslike}" == "darwin" ]]; then
+  mkdir -p "${HOME}/.local/bin"
   if [[ "${run_sudo}" == "true" ]]; then
     if brew --version; then
       printf "\e[33mFound brew, skip install homebrew.\e[0m\n"
@@ -36,6 +41,8 @@ if [[ "${oslike}" == "darwin" ]]; then
   else
     printf "\e[31mSkipping homebrew and package installation\e[0m\n"
   fi
+
+  curl -fsSL https://github.com/facebook/dotslash/releases/latest/download/dotslash-macos.tar.gz | tar fxz - -C "${HOME}/.local/bin"
 fi
 
 printf "\e[32mCheck basic packages done.\e[0m\n"
