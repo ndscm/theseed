a720db18b4885380122cdaea12ae5a5cecedebfb
Nagi <nagi@ndscm.com>
2026-06-18 08:55:23 -0700

    seed: sfe: use openid provider
    Change-uuid: b2128be4-a493-4f0c-9c6a-bb70a027ac96
    Side-effect-of-change-uuid: b237673f-b983-4847-aac9-7e09641fc482
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/cloud/sfe/route/golinkroute/golink_route.go
+++ seed/cloud/sfe/route/golinkroute/golink_route.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	"github.com/ndscm/theseed/seed/infra/auth/go/authfe"
-	"github.com/ndscm/theseed/seed/infra/auth/go/loginopenid"
 	"github.com/ndscm/theseed/seed/infra/auth/go/openid"
 	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
 	"github.com/ndscm/theseed/seed/infra/flag/go/seedflag"
@@ -47,7 +46,7 @@ func CreateGolinkRoute(transport http.RoundTripper) (*GolinkRoute, error) {
 		}
 		clientSecret = strings.TrimSpace(string(clientSecretBytes))
 	}
-	provider := loginopenid.NewUserOpenidProvider(
+	provider := openid.NewOpenidProvider(
 		openid.NewOpenidClient(discoveryUrl, clientId, clientSecret), "golink_")
 	authHandler := authfe.NewAuthHandler(provider)
 	serverUrl, err := url.Parse(flagGolinkServiceServer.Get())
