32eb0fba6f09dc55be5d5e3afcc34fe9be7e404d
Nagi <nagi@ndscm.com>
2026-06-19 11:16:48 -0700

    seed: login: go: switch to external storage token source for silicon login
    Change-uuid: 05fecc87-2bf6-4451-805b-772f95b68119
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/cloud/login/go/siliconlogin/siliconlogin.go
+++ seed/cloud/login/go/siliconlogin/siliconlogin.go
@@ -9,14 +9,15 @@ import (
 	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
 	"github.com/ndscm/theseed/seed/infra/flag/go/seedflag"
 	"github.com/ndscm/theseed/seed/infra/http/go/seedbearer"
+	"golang.org/x/oauth2"
 )
 
 var flagSiliconRefreshTokenFile = seedflag.DefineString("silicon_refresh_token_file", "", "Path to file containing refresh token for Silicon login.")
 
 type SiliconSession struct {
-	mutex    sync.Mutex
-	token    *openid.MemoryTokenStorage
-	provider *openid.OpenidProvider
+	mutex       sync.Mutex
+	provider    *openid.OpenidProvider
+	tokenSource oauth2.TokenSource
 }
 
 var session = SiliconSession{}
@@ -25,29 +26,39 @@ func SiliconLogin(ctx context.Context) (context.Context, error) {
 	session.mutex.Lock()
 	defer session.mutex.Unlock()
 
-	if session.token == nil || session.provider == nil {
+	if session.tokenSource == nil || session.provider == nil {
 		siliconRefreshTokenFile := flagSiliconRefreshTokenFile.Get()
 		if siliconRefreshTokenFile != "" {
 			refreshToken, err := os.ReadFile(siliconRefreshTokenFile)
 			if err != nil {
 				return ctx, seederr.Wrap(err)
 			}
-			session.token = openid.NewMemoryTokenStorage(
-				map[string]string{"refresh_token": string(refreshToken)},
-			)
+			initial := &oauth2.Token{
+				RefreshToken: string(refreshToken),
+			}
 
 			discoveryUrl := openid.OpenidDiscoveryUrlFlag()
-			base := openid.NewOpenidClient(discoveryUrl, "silicon-prod", "")
-			session.provider = openid.NewOpenidProvider(base, "")
+			openidClient := openid.NewOpenidClient(discoveryUrl, "silicon-prod", "")
+			session.provider = openid.NewOpenidProvider(openidClient, "")
+			tokenSource, err := session.provider.WrapExternalTokenStorage(
+				ctx, nil, nil, initial,
+			)
+			if err != nil {
+				return ctx, seederr.Wrap(err)
+			}
+			session.tokenSource = tokenSource
 		}
 	}
 
-	if session.token != nil && session.provider != nil {
-		accessToken, err := session.provider.AccessToken(ctx, session.token)
+	if session.tokenSource != nil && session.provider != nil {
+		token, err := session.tokenSource.Token()
 		if err != nil {
 			return ctx, seederr.Wrap(err)
 		}
-		ctx = seedbearer.WithBearer(ctx, accessToken)
+		if !token.Valid() {
+			return ctx, seederr.WrapErrorf("token is invalid")
+		}
+		ctx = seedbearer.WithBearer(ctx, token.AccessToken)
 	}
 	return ctx, nil
 }
