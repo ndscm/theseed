6f56c9030b4b780150d0873edee7be818a4fe75f
Nagi <nagi@ndscm.com>
2026-06-19 11:30:59 -0700

    seed: auth: go: remove raw provider access token access
    Change-uuid: 5c17dc28-f4e9-4876-b7f9-2cc8e9561227
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/infra/auth/go/openid/openid_provider.go
+++ seed/infra/auth/go/openid/openid_provider.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"io"
 	"net/http"
-	"time"
 
 	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
 	"golang.org/x/oauth2"
@@ -91,59 +90,6 @@ func (provider *OpenidProvider) WrapExternalTokenStorage(
 		return nil, seederr.Wrap(err)
 	}
 	return userTokenSource, nil
-}
-
-func (provider *OpenidProvider) AccessToken(
-	ctx context.Context,
-	storage ExternalTokenStorage,
-) (string, error) {
-	oauth2Config, err := provider.GetOauth2Config(ctx, "", nil)
-	if err != nil {
-		return "", seederr.Wrap(err)
-	}
-	accessToken, err := storage.Get(ctx, provider.prefix+"access_token")
-	if err != nil {
-		return "", seederr.Wrap(err)
-	}
-	refreshToken, err := storage.Get(ctx, provider.prefix+"refresh_token")
-	if err != nil {
-		return "", seederr.Wrap(err)
-	}
-	expiryString, err := storage.Get(ctx, provider.prefix+"expiry")
-	if err != nil {
-		return "", seederr.Wrap(err)
-	}
-	expiry, err := time.Parse(time.RFC3339Nano, expiryString)
-	if err != nil {
-		expiry = time.Now().Add(-time.Minute)
-	}
-	initialToken := &oauth2.Token{
-		AccessToken:  accessToken,
-		RefreshToken: refreshToken,
-		Expiry:       expiry,
-	}
-	tokenSource, err := createExternalTokenStorageTokenSource(
-		ctx, provider.prefix, oauth2Config, storage, initialToken,
-	)
-	if err != nil {
-		return "", seederr.Wrap(err)
-	}
-	token, err := tokenSource.Token()
-	if err != nil {
-		return "", seederr.Wrap(err)
-	}
-	return token.AccessToken, nil
-}
-
-func (provider *OpenidProvider) Authorization(
-	ctx context.Context,
-	storage ExternalTokenStorage,
-) string {
-	accessToken, err := provider.AccessToken(ctx, storage)
-	if err != nil {
-		return ""
-	}
-	return "Bearer " + accessToken
 }
 
 func (provider *OpenidProvider) Client(
