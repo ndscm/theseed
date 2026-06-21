d9bcb4a6cb607292d4efd0809433f762b210c92b
Nagi <nagi@ndscm.com>
2026-06-21 04:54:02 -0700

    seed: ndscm: add nd testable command
    Change-uuid: 8b77e1e9-be11-4b62-bb98-841253758989
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/devprod/ndscm/cli/nd_testable.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/ndscm/theseed/seed/devprod/ndscm/clientcore"
+	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
+	"github.com/ndscm/theseed/seed/infra/flag/go/seedflag"
+)
+
+func ndTestable(args []string) error {
+	seedflag.Finalize(args)
+	commit := ""
+	if len(args) > 1 {
+		return seederr.WrapErrorf("nd-testable usage: ndscm testable [commit]")
+	}
+	if len(args) == 1 {
+		commit = args[0]
+	}
+
+	cc := &clientcore.ClientCore{}
+	err := cc.Initialize()
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+
+	testable, err := cc.CheckTestable(commit)
+	if err != nil {
+		return seederr.Wrap(err)
+	}
+	if !testable {
+		return seederr.WrapErrorf("commit is not testable.")
+	}
+	return nil
+}
