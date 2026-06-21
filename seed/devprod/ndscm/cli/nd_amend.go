845b33723793bbc8deec12eddb1d1d6e7eda09a5
Nagi <nagi@ndscm.com>
2026-06-21 03:53:56 -0700

    seed: ndscm: add side effect search for nd amend
    Change-uuid: b8a44aae-6323-4ccd-8f38-34dc49e2648b
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/ndscm/cli/nd_amend.go
+++ seed/devprod/ndscm/cli/nd_amend.go
@@ -7,8 +7,9 @@ import (
 )
 
 type ndAmendFlags struct {
-	breakText   *seedflag.StringFlag
-	migrateText *seedflag.StringFlag
+	breakText    *seedflag.StringFlag
+	migrateText  *seedflag.StringFlag
+	sideEffectOf *seedflag.StringFlag
 }
 
 func parseNdAmendFlags(args []string) (ndAmendFlags, []string, error) {
@@ -16,6 +17,7 @@ func parseNdAmendFlags(args []string) (ndAmendFlags, []string, error) {
 	cmdFlags := ndAmendFlags{}
 	cmdFlags.breakText = cf.DefineString("break", "", "Breaking change description")
 	cmdFlags.migrateText = cf.DefineString("migrate", "", "Migration instruction")
+	cmdFlags.sideEffectOf = cf.DefineString("side-effect-of", "", "Change UUID this commit is a side effect of")
 	cmdArgs, err := cf.Parse(args)
 	if err != nil {
 		return cmdFlags, nil, seederr.Wrap(err)
@@ -30,7 +32,7 @@ func ndAmend(args []string) error {
 		return seederr.Wrap(err)
 	}
 	if len(cmdArgs) != 0 {
-		return seederr.WrapErrorf("nd-amend usage: nd amend --break=<text> --migrate=<text>")
+		return seederr.WrapErrorf("nd-amend usage: nd amend --side-effect-of=<uuid>|break --break=<text> --migrate=<text>")
 	}
 	cc := &clientcore.ClientCore{}
 	err = cc.Initialize()
@@ -38,8 +40,9 @@ func ndAmend(args []string) error {
 		return seederr.Wrap(err)
 	}
 	err = cc.NdAmend(clientcore.NdAmendOptions{
-		Break:   cmdFlags.breakText.Get(),
-		Migrate: cmdFlags.migrateText.Get(),
+		Break:        cmdFlags.breakText.Get(),
+		Migrate:      cmdFlags.migrateText.Get(),
+		SideEffectOf: cmdFlags.sideEffectOf.Get(),
 	})
 	if err != nil {
 		return seederr.Wrap(err)
--- seed/devprod/ndscm/clientcore/nd_amend.go
+++ seed/devprod/ndscm/clientcore/nd_amend.go
@@ -1,19 +1,55 @@
 package clientcore
 
 import (
+	"strings"
+
+	"github.com/google/uuid"
 	"github.com/ndscm/theseed/seed/devprod/ndscm/scm"
 	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
 )
 
 type NdAmendOptions struct {
-	Break   string
-	Migrate string
+	Break        string
+	Migrate      string
+	SideEffectOf string
 }
 
 func NdAmend(scmProvider scm.Provider, options NdAmendOptions) error {
-	if options.Break == "" && options.Migrate == "" {
-		return seederr.WrapErrorf("nd-amend requires at least one of --break or --migrate")
+	if options.Break == "" && options.Migrate == "" && options.SideEffectOf == "" {
+		return seederr.WrapErrorf("nd-amend requires at least one of --break, --migrate, or --side-effect-of")
 	}
+
+	// Side-effect-of-change-uuid is usually added before the new break and migrate metadata.
+	if options.SideEffectOf != "" {
+		sideEffectOf := strings.TrimSpace(strings.ToLower(options.SideEffectOf))
+		switch sideEffectOf {
+		case "break":
+			breakCommitId, err := scmProvider.SearchExtendedMetadata("HEAD^", "Break", "")
+			if err != nil {
+				return seederr.Wrap(err)
+			}
+			breakCommitMetadata, err := scmProvider.GetCommitMetadata(breakCommitId)
+			if err != nil {
+				return seederr.Wrap(err)
+			}
+			parsedUuid, err := uuid.Parse(breakCommitMetadata.ChangeUuid)
+			if err != nil {
+				return seederr.WrapErrorf("break commit does not have a valid change UUID. commit=%s", breakCommitId)
+			}
+			sideEffectOf = parsedUuid.String()
+		default:
+			parsedUuid, err := uuid.Parse(options.SideEffectOf)
+			if err != nil {
+				return seederr.WrapErrorf("side effect is not a valid UUID. sideEffectOf='%s'", options.SideEffectOf)
+			}
+			sideEffectOf = parsedUuid.String()
+		}
+		err := scmProvider.AmendAppendExtendedMetadata("Side-effect-of-change-uuid", sideEffectOf)
+		if err != nil {
+			return seederr.Wrap(err)
+		}
+	}
+
 	if options.Break != "" {
 		if options.Break == "lock" && options.Migrate == "" {
 			options.Migrate = "update lock"
