c68e008bef173b1c42638a3be2be8fb46f9203a1
Nagi <nagi@ndscm.com>
2026-06-23 10:38:18 -0700

    seed: buildinfo: ts: embed buildinfo in lib
    Change-uuid: a8b5e75c-4837-4821-8f66-5491c2ed5b58
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/infra/buildinfo/ts/src/status.d.ts
@@ -0,0 +1,9 @@
+declare module "*/stable-status" {
+  const content: string
+  export default content
+}
+
+declare module "*/volatile-status" {
+  const content: string
+  export default content
+}
