466f714319fab860426cb67c343c3d187815870a
Nagi <nagi@ndscm.com>
2026-06-24 06:13:38 -0700

    seed: smd: workstation: maintain rust packages
    Change-uuid: 242508bf-343f-4217-8688-57e147c174da
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/devprod/smd/workstation/maintain-46-user-rust.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
+
+if [[ ",${maintain_scopes}," == *",user,"* ]]; then
+    printf "\e[34m[user] Checking rust tools...\e[0m\n"
+
+    if [[ "${oslike}" == "debian" || "${oslike}" == "darwin" ]]; then
+        cat <<EOF >>"${HOME}/.managed_profile.tmp"
+
+## Rust
+
+if [[ -f "\${HOME}/.cargo/env" ]]; then
+  . "\${HOME}/.cargo/env"
+fi
+EOF
+        rustup default stable
+        rustup update stable
+    fi
+
+    printf "\e[32m[user] Check rust tools done.\e[0m\n"
+fi
