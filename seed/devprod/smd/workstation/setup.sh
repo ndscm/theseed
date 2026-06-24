4005ac586ae0e1a0ac5f686e62c45ff2c9909292
Nagi <nagi@ndscm.com>
2026-06-24 01:43:40 -0700

    seed: smd: workstation: use ndscm connect to clone monorepo
    Change-uuid: fa5e9e45-f6b5-458b-93e3-3b54aada4d2a
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/smd/workstation/setup.sh
+++ seed/devprod/smd/workstation/setup.sh
@@ -204,32 +204,22 @@ if [[ ",${maintain_scopes}," == *",user,"* ]]; then
   printf "\e[34m[user] Checking theseed monorepo...\e[0m\n"
 
   if [[ "${oslike}" == "debian" || "${oslike}" == "darwin" ]]; then
-    mkdir -p "${HOME}/theseed"
+    mkdir -p "${HOME}/ndscm"
 
-    if [[ -d "${HOME}/theseed/theseed.git" && -d "${HOME}/theseed/main" ]]; then
-      printf "\e[33mFound existing theseed monorepo, skip clone.\e[0m\n"
+    if [[ -d "${HOME}/ndscm/theseed" ]]; then
+      printf "\e[33mFound existing theseed monorepo, skip connect.\e[0m\n"
     else
-      printf "\e[34mCloning theseed monorepo...\e[0m\n"
-      git clone --bare --single-branch \
-        --config "core.logallrefupdates=true" \
-        --config "remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*" \
-        git@github.com:ndscm/theseed.git \
-        "${HOME}/theseed/theseed.git"
-      git --git-dir "${HOME}/theseed/theseed.git" worktree add -B main "${HOME}/theseed/main" origin/main
+      ndscm connect \
+        --repos-home "${HOME}/ndscm" \
+        theseed "git@github.com:ndscm/theseed.git"
     fi
-    git --git-dir "${HOME}/theseed/theseed.git" config user.name "${ND_USER_DISPLAY_NAME}"
-    git --git-dir "${HOME}/theseed/theseed.git" config user.email "${ND_USER_HANDLE}@${ND_USER_DOMAIN}"
-
-    git -C "${HOME}/theseed/main" fetch --all --prune
-    git -C "${HOME}/theseed/main" rebase origin/main
   fi
 
-  export ND_MONOREPO_HOME="${HOME}/theseed"
-  export ND_MONOREPO_GIT_DIR="${ND_MONOREPO_HOME}/theseed.git"
+  export ND_REPOS_HOME="${HOME}/ndscm"
 
   printf "\e[32m[user] Check theseed monorepo done.\e[0m\n"
 fi
 
 # seed/devprod/smd/workstation/maintain.sh
 
-exec "${HOME}/theseed/main/seed/devprod/smd/workstation/maintain.sh" "${maintain_scopes}"
+exec "${ND_REPOS_HOME}/theseed/main/seed/devprod/smd/workstation/maintain.sh" "${maintain_scopes}"
