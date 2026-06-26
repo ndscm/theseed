031b930a16dab4a9ad9a2b7f19aa4289c95d41d8
Nagi <nagi@ndscm.com>
2026-06-26 06:05:38 -0700

    seed: smd: workstation: rerun shell format
    Change-uuid: cb3e8933-3ad8-419b-b3b3-5294a1d378b8
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/smd/workstation/maintain-61-user-shortcut.sh
+++ seed/devprod/smd/workstation/maintain-61-user-shortcut.sh
@@ -4,17 +4,17 @@ set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 
 if [[ ",${maintain_scopes}," == *",user,"* ]]; then
-    if [[ "${oslike}" == "debian" || "${oslike}" == "darwin" ]]; then
-        cat <<EOF >>"${HOME}/.managed_shrc.tmp"
+  if [[ "${oslike}" == "debian" || "${oslike}" == "darwin" ]]; then
+    cat <<EOF >>"${HOME}/.managed_shrc.tmp"
 
 ## Shortcuts
 
 function main { cd "\${ND_REPOS_HOME}/theseed/main"; }
 function dev { cd "\${ND_REPOS_HOME}/theseed"; nd dev; }
 EOF
-    fi
+  fi
 
-    if [[ "${oslike}" == "debian" || "${oslike}" == "darwin" ]]; then
-        ln -s -f -n "${ND_REPOS_HOME}/theseed/main/seed/devprod/setproxy.sh" "${HOME}/.local/bin/setproxy"
-    fi
+  if [[ "${oslike}" == "debian" || "${oslike}" == "darwin" ]]; then
+    ln -s -f -n "${ND_REPOS_HOME}/theseed/main/seed/devprod/setproxy.sh" "${HOME}/.local/bin/setproxy"
+  fi
 fi
--- seed/devprod/smd/workstation/maintain-99-user-finalize.sh
+++ seed/devprod/smd/workstation/maintain-99-user-finalize.sh
@@ -4,8 +4,8 @@ set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 
 if [[ ",${maintain_scopes}," == *",user,"* ]]; then
-    if [[ "${oslike}" == "debian" || "${oslike}" == "darwin" ]]; then
-        cat <<EOF >>"${HOME}/.managed_profile.tmp"
+  if [[ "${oslike}" == "debian" || "${oslike}" == "darwin" ]]; then
+    cat <<EOF >>"${HOME}/.managed_profile.tmp"
 
 ## Personal Profile
 
@@ -13,7 +13,7 @@ if [ -f "\${HOME}/.personal_profile" ]; then
   source "\${HOME}/.personal_profile"
 fi
 EOF
-        cat <<EOF >>"${HOME}/.managed_shrc.tmp"
+    cat <<EOF >>"${HOME}/.managed_shrc.tmp"
 
 ## Personal shrc
 
@@ -21,10 +21,10 @@ if [ -f "\${HOME}/.personal_shrc" ]; then
   source "\${HOME}/.personal_shrc"
 fi
 EOF
-    fi
+  fi
 
-    mv -f "${HOME}/.managed_profile.tmp" "${HOME}/.managed_profile"
-    mv -f "${HOME}/.managed_shrc.tmp" "${HOME}/.managed_shrc"
+  mv -f "${HOME}/.managed_profile.tmp" "${HOME}/.managed_profile"
+  mv -f "${HOME}/.managed_shrc.tmp" "${HOME}/.managed_shrc"
 
-    printf "\e[32mDone. Please restart the terminal to load new environments.\e[0m\n"
+  printf "\e[32mDone. Please restart the terminal to load new environments.\e[0m\n"
 fi
