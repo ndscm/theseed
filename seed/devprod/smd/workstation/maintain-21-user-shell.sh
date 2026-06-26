8622af5f1d3ac1154296e33cc00e3bf0c0aa9876
Nagi <nagi@ndscm.com>
2026-06-26 06:26:43 -0700

    seed: smd: workstation: ask user to chsh
    Change-uuid: 60f3046f-c42c-4cd8-b00f-45adadc2861c
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/smd/workstation/maintain-21-user-shell.sh
+++ seed/devprod/smd/workstation/maintain-21-user-shell.sh
@@ -13,8 +13,20 @@ if [[ ",${maintain_scopes}," == *",user,"* ]]; then
     cp "${HOME}/.oh-my-zsh/templates/zshrc.zsh-template" "${HOME}/.zshrc"
     sed -i.bak 's/ZSH_THEME=".*"/ZSH_THEME="agnoster"/' "${HOME}/.zshrc"
     printf '\nunsetopt SHARE_HISTORY\n' >>"${HOME}/.zshrc"
-    if [[ "${SHELL}" != "/bin/zsh" ]]; then
-      chsh -s /bin/zsh
+  fi
+
+  login_shell=""
+  if [[ "${oslike}" == "debian" ]]; then
+    login_shell="$(getent passwd "$(id -un)" | cut -d: -f7)"
+  fi
+  if [[ "${oslike}" == "darwin" ]]; then
+    login_shell="$(dscl . -read "/Users/$(id -un)" UserShell | awk '{print $2}')"
+  fi
+
+  if [[ "${login_shell}" != */zsh ]]; then
+    read -r -p "Change login shell to $(which zsh)? [y/N] " reply
+    if [[ "${reply}" == [yY] ]]; then
+      chsh -s "$(which zsh)"
     fi
   fi
 
