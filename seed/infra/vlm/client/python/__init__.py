7311481abaf45e820c6e47ae4b1e76838d77be0b
Nagi <nagi@ndscm.com>
2026-06-26 00:16:46 -0700

    seed: vlm: client: use seed init from init package
    Change-uuid: 18cb9895-f09b-4e48-9629-75beff950eba
    Side-effect-of-change-uuid: d8daecee-3f9b-4a69-a93d-2d9b1420bb2f
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/infra/vlm/client/python/__init__.py
+++ seed/infra/vlm/client/python/__init__.py
@@ -1,6 +1,6 @@
 import seed.infra.billing.python.llm_bill as llm_bill
 import seed.infra.flag.python.seed_flag as seed_flag
-import seed.infra.python.seed_init as seed_init
+import seed.infra.init.python.seed_init as seed_init
 import seed.infra.typing.python.seed_typing as seed_typing
 import seed.infra.vlm.client.python.vlm_client as vlm_client
 
