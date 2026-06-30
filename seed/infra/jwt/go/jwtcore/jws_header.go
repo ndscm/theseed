61f0960b3fa8f0b80b5c89d30cac3c21915515f6
Nagi <nagi@ndscm.com>
2026-06-30 10:10:01 -0700

    seed: jwt: go: extract jwt types into jwtcore package
    Change-uuid: 52a7fc75-5252-443d-b68c-9c289284156d
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/infra/jwt/go/jwtcore/jws_header.go
@@ -0,0 +1,28 @@
+package jwtcore
+
+import (
+	"crypto"
+
+	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
+)
+
+// See RFC 7515 for JWS structure
+// https://datatracker.ietf.org/doc/html/rfc7515
+type JwsHeader struct {
+	Alg string `json:"alg"`
+	Kid string `json:"kid"`
+	Typ string `json:"typ"`
+}
+
+func (h *JwsHeader) LookupHashFunction() (crypto.Hash, error) {
+	switch h.Alg {
+	case "RS256", "ES256":
+		return crypto.SHA256, nil
+	case "RS384", "ES384":
+		return crypto.SHA384, nil
+	case "RS512", "ES512":
+		return crypto.SHA512, nil
+	default:
+		return 0, seederr.WrapErrorf("unsupported JWT algorithm %q", h.Alg)
+	}
+}
