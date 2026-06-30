ecadc0fc3d256492cb3e40cadf92fe3e9c086a1d
Nagi <nagi@ndscm.com>
2026-06-30 05:06:12 -0700

    seed: stuff: database: load database secret with secret flag
    Change-uuid: c6ba9c46-06b7-46a6-9fab-441d02c8632e
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/office/stuff/database/stuffdb/stuff_database_client.go
+++ seed/office/stuff/database/stuffdb/stuff_database_client.go
@@ -3,9 +3,6 @@ package stuffdb
 import (
 	"context"
 	"net/url"
-	"os"
-	"path/filepath"
-	"strings"
 
 	"entgo.io/ent/dialect"
 	_ "github.com/lib/pq"
@@ -18,7 +15,10 @@ import (
 var flagStuffDatabase = seedflag.DefineString("stuff_database", "postgres://127.0.0.1:5432/stuff", "Database connection URL")
 var flagStuffDatabaseDebug = seedflag.DefineBool("stuff_database_debug", false, "Enable debug mode for database")
 var flagStuffDatabaseLogin = seedflag.DefineString("stuff_database_login", "stuff", "Database login user")
-var flagStuffDatabaseSecretFile = seedflag.DefineString("stuff_database_secret_file", "user-secret://stuff/STUFF_DATABASE_SECRET/latest", "Path to database password file")
+var flagStuffDatabaseSecret = seedflag.DefineSecret(
+	"stuff_database_secret",
+	"Stuff database password",
+)
 
 func Open(ctx context.Context) (*ent.Client, error) {
 	connectUrl, err := url.Parse(flagStuffDatabase.Get())
@@ -26,20 +26,12 @@ func Open(ctx context.Context) (*ent.Client, error) {
 		return nil, seederr.Wrap(err)
 	}
 	seedlog.Debugf("Connecting to database: %s", connectUrl.Host+connectUrl.Path)
-	databaseSecretPath := flagStuffDatabaseSecretFile.Get()
-	if strings.HasPrefix(databaseSecretPath, "~/") {
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			return nil, seederr.Wrap(err)
-		}
-		databaseSecretPath = filepath.Join(homeDir, databaseSecretPath[2:])
-	}
-	databaseSecret, err := os.ReadFile(databaseSecretPath)
+	databaseLogin := flagStuffDatabaseLogin.Get()
+	databaseSecret, err := flagStuffDatabaseSecret.LoadString()
 	if err != nil {
 		return nil, seederr.Wrap(err)
 	}
-	databaseSecret = []byte(strings.TrimSpace(string(databaseSecret)))
-	connectUrl.User = url.UserPassword(flagStuffDatabaseLogin.Get(), string(databaseSecret))
+	connectUrl.User = url.UserPassword(databaseLogin, databaseSecret)
 	db, err := ent.Open(dialect.Postgres, connectUrl.String())
 	if err != nil {
 		return nil, seederr.Wrap(err)
