d9adca3f68d1a39a8fb98894ad9437d2b7ff22b1
Nagi <nagi@ndscm.com>
2026-06-30 04:18:54 -0700

    seed: flag: go: extract string flag to separate file
    Change-uuid: 1742aae5-24e6-40b5-8877-343283acd312
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/infra/flag/go/seedflag/string_flag.go
@@ -0,0 +1,30 @@
+package seedflag
+
+type StringFlag struct {
+	FlagItem
+	value string
+}
+
+func (f *StringFlag) Set(s string) error {
+	f.value = s
+	return nil
+}
+
+func (f *StringFlag) Get() string {
+	return f.value
+}
+
+func (f *StringFlag) String() string {
+	return f.value
+}
+
+var _ FlagDefinition = (*StringFlag)(nil)
+
+func newStringFlag(defaultValue string, usage string) *StringFlag {
+	return &StringFlag{
+		FlagItem: FlagItem{
+			usage: usage,
+		},
+		value: defaultValue,
+	}
+}
