0cb8c43eab10eb787ccf283a7c5bd4666ca2862e
Nagi <nagi@ndscm.com>
2026-07-01 01:51:43 -0700

    seed: sfe: kurisuroute: support private key jwt
    Change-uuid: 8aef17c4-88eb-43a7-a36d-69c873fdf95c
    Break: create kurisu route
    Migrate: pass sfe openid client
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/cloud/sfe/route/kurisuroute/kurisu_route.go
+++ seed/cloud/sfe/route/kurisuroute/kurisu_route.go
@@ -5,19 +5,30 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"strings"
+	"time"
 
+	"github.com/ndscm/theseed/seed/cloud/sfe/signedjwt"
 	"github.com/ndscm/theseed/seed/infra/auth/go/authfe"
 	"github.com/ndscm/theseed/seed/infra/auth/go/openid"
 	"github.com/ndscm/theseed/seed/infra/error/go/seederr"
 	"github.com/ndscm/theseed/seed/infra/flag/go/seedflag"
 )
 
-var flagKurisuServiceServer = seedflag.DefineString("kurisu_service_server", "", "URL of Kurisu service server")
-var flagKurisuOpenidDiscoveryUrl = seedflag.DefineString("kurisu_openid_discovery_url", "http://127.0.0.1:8080/realms/ndscm/.well-known/openid-configuration", "Discovery URL of Kurisu OpenID provider")
-var flagKurisuOpenidClientId = seedflag.DefineString("kurisu_openid_client_id", "", "Client ID for Kurisu OpenID provider")
+var flagKurisuServiceServer = seedflag.DefineString(
+	"kurisu_service_server", "",
+	"URL of the Kurisu service server.",
+)
+var flagKurisuOpenidDiscoveryUrl = seedflag.DefineString(
+	"kurisu_openid_discovery_url", "",
+	"Discovery URL for the Kurisu OpenID provider, used only when a client ID is set. Defaults to the standard OpenID discovery URL when left empty.",
+)
+var flagKurisuOpenidClientId = seedflag.DefineString(
+	"kurisu_openid_client_id", "",
+	"Client ID for the Kurisu OpenID provider. Falls back to the SFE OpenID client when left empty.",
+)
 var flagKurisuOpenidClientSecret = seedflag.DefineSecret(
 	"kurisu_openid_client_secret",
-	"Client secret for Kurisu OpenID provider",
+	"Client secret for the Kurisu OpenID provider. When left empty, a signed assertion is attempted instead.",
 )
 
 type KurisuRoute struct {
@@ -36,28 +47,46 @@ func (p *KurisuRoute) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 var _ http.Handler = (*KurisuRoute)(nil)
 
-func CreateKurisuRoute(transport http.RoundTripper) (*KurisuRoute, error) {
+func CreateKurisuRoute(sfeOpenidClient *openid.OpenidClient) (*KurisuRoute, error) {
+	// Create Auth Handler
 	discoveryUrl := flagKurisuOpenidDiscoveryUrl.Get()
+	if discoveryUrl == "" {
+		discoveryUrl = openid.OpenidDiscoveryUrlFlag()
+	}
 	clientId := flagKurisuOpenidClientId.Get()
 	clientSecret, err := flagKurisuOpenidClientSecret.LoadString()
 	if err != nil {
 		return nil, seederr.Wrap(err)
 	}
-	provider := openid.NewOpenidProvider(
-		openid.NewOpenidClient(discoveryUrl, clientId, clientSecret), "kurisu_")
+	client, err := signedjwt.WrapOpenidClient(
+		discoveryUrl, clientId, clientSecret, sfeOpenidClient,
+	)
+	if err != nil {
+		return nil, seederr.Wrap(err)
+	}
+	provider := openid.NewOpenidProvider(client, "kurisu_")
 	authHandler := authfe.NewAuthHandler(provider)
+
+	// Create Reverse Proxy
 	serverUrl, err := url.Parse(flagKurisuServiceServer.Get())
 	if err != nil {
 		return nil, seederr.Wrap(err)
 	}
 	reverseProxy := &httputil.ReverseProxy{
-		Transport: transport,
+		Transport: &http.Transport{
+			Proxy:               nil,
+			MaxIdleConns:        200,
+			MaxIdleConnsPerHost: 100,
+			IdleConnTimeout:     10 * time.Second,
+		},
 		Rewrite: func(r *httputil.ProxyRequest) {
 			r.SetURL(serverUrl)
 			r.SetXForwarded()
 		},
 	}
 	next := authfe.InterceptSessionAuthorizationMiddleware(reverseProxy, provider)
+
+	// Create Kurisu Route
 	route := &KurisuRoute{
 		authHandler: authHandler,
 		next:        next,
