0d8be789aefb0e2edd914169076f8eefda4afd7e
Nagi <nagi@ndscm.com>
2026-07-10 10:31:50 -0700

    seed: terminal: add tty terminal proto
    Change-uuid: f0a465a4-aca0-4e8f-a30d-1b850fc0535b
    Break: lock
    Migrate: update lock
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/infra/terminal/proto/DIR.ndscm.ts
@@ -0,0 +1,19 @@
+import { type DirConfig } from "@//seed/devprod/ndscm/config/DIR"
+
+export default {
+  bootstrap: {
+    proto: {
+      target: [
+        // sort
+        "terminal_pb.d.ts",
+        "terminal_pb.js",
+        "terminalpb",
+      ],
+      watch: "\\.proto$",
+      bazel: {
+        build: ":bootstrap",
+        run: "{{BAZEL_RUN}}",
+      },
+    },
+  },
+} satisfies DirConfig
