e6ef582e3ed7a60e0084b7247a5c3b72905eda8b
Nagi <nagi@ndscm.com>
2026-07-10 11:24:17 -0700

    seed: kurisu: proxy hooin invade service in server
    Change-uuid: 75c28839-4ecb-436d-933b-8a42e122bb68
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/kurisu/server/start.sh
+++ seed/newtype/kurisu/server/start.sh
@@ -6,14 +6,17 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 stack=${1:-"local"}
 
 hooin_dictate_service_server="http://127.0.0.1:4664"
+hooin_invade_service_server="http://127.0.0.1:4664"
 hooin_roster_service_server="http://127.0.0.1:4664"
 
 if [[ "$stack" == "prod" ]]; then
   hooin_dictate_service_server="https://hooin.ndscm.biz"
+  hooin_invade_service_server="https://hooin.ndscm.biz"
   hooin_roster_service_server="https://hooin.ndscm.biz"
 fi
 
 bazel run //seed/newtype/kurisu/server -- \
   --hooin_dictate_service_server="${hooin_dictate_service_server}" \
+  --hooin_invade_service_server="${hooin_invade_service_server}" \
   --hooin_roster_service_server="${hooin_roster_service_server}" \
   --verbose
