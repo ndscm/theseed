0769d2f3c533cfa5ad12caaecb4584bed25c2a96
Nagi <nagi@ndscm.com>
2026-07-18 09:33:34 -0700

    seed: keycloak: client: get user representation
    Change-uuid: ab33b0ef-30e7-42a0-8c1f-fa7c6b23e3e3
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/cloud/keycloak/client/go/keycloak/user.go
+++ seed/cloud/keycloak/client/go/keycloak/user.go
@@ -1,7 +1,21 @@
 package keycloak
 
+// UserRepresentation is the Keycloak admin REST representation of a realm user. Only
+// the fields the seed uses are declared; Keycloak returns many more.
 type UserRepresentation struct {
 	Id string `json:"id"`
 
 	Username string `json:"username"`
+
+	FirstName string `json:"firstName,omitempty"`
+
+	LastName string `json:"lastName,omitempty"`
+
+	Email string `json:"email,omitempty"`
+
+	// Attributes holds the user's custom attributes. Each attribute maps to a
+	// list of values, mirroring the Keycloak representation.
+	Attributes map[string][]string `json:"attributes,omitempty"`
+
+	Enabled bool `json:"enabled"`
 }
