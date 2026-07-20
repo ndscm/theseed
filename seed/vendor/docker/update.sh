be5d0f28b57eff2d42f387c81c0942bb35c7402c
Nagi <nagi@ndscm.com>
2026-07-20 10:46:26 -0700

    seed: vendor: docker: update dotslash with explicit format and replacement
    Change-uuid: 371d8b9e-a30c-4ede-a7e0-a9817ceb8d10
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/docker/update.sh
+++ seed/vendor/docker/update.sh
@@ -3,10 +3,10 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="29.5.3"
+tag="${1:-"29.5.3"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/docker/docker.dotslash.json" \
-  --tag="${tag}" \
+  --replace "TAG=${tag}" \
   >./seed/vendor/docker/bin/docker
 chmod +x ./seed/vendor/docker/bin/docker
