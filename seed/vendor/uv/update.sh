40dabc930aec7c1dc4f9cab49b5251f3ed11f6df
Nagi <nagi@ndscm.com>
2026-07-20 13:59:02 -0700

    seed: vendor: uv: update with explicit dotslash extension
    Change-uuid: 8d9f8355-3e18-4317-8f6b-c2e48cfd7ad8
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/uv/update.sh
+++ seed/vendor/uv/update.sh
@@ -7,12 +7,12 @@ tag="${1:-"0.11.21"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/uv/uv.dotslash.json" \
-  --replace "TAG=${tag}" \
-  >./seed/vendor/uv/bin/uv
-chmod +x ./seed/vendor/uv/bin/uv
-
-bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/uv/uvx.dotslash.json" \
   --replace "TAG=${tag}" \
-  >./seed/vendor/uv/bin/uvx
-chmod +x ./seed/vendor/uv/bin/uvx
+  --outdir "$(pwd)/seed/vendor/uv/bin"
+
+chmod +x ./seed/vendor/uv/bin/uv.dotslash
+chmod +x ./seed/vendor/uv/bin/uvx.dotslash
+
+ln -s -f uv.dotslash ./seed/vendor/uv/bin/uv
+ln -s -f uvx.dotslash ./seed/vendor/uv/bin/uvx
