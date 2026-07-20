fcb5f3b9a0bf8f65da8f203baa1fb7d9afb6f17b
Nagi <nagi@ndscm.com>
2026-07-20 14:41:35 -0700

    contrib: vendor: perforce: update dotslash entry with explicit dotslash extension
    Change-uuid: c09ec881-ff66-456c-89ad-4009a2a6aa5a
    Signed-off-by: Nagi <nagi@ndscm.com>

--- contrib/vendor/perforce/update.sh
+++ contrib/vendor/perforce/update.sh
@@ -3,10 +3,13 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="r26.1"
+tag="${1:-"r26.1"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/contrib/vendor/perforce/p4merge.dotslash.json" \
-  --tag="${tag}" \
-  >./contrib/vendor/perforce/bin/p4merge
-chmod +x ./contrib/vendor/perforce/bin/p4merge
+  --replace "TAG=${tag}" \
+  --outdir "$(pwd)/contrib/vendor/perforce/bin"
+
+chmod +x ./contrib/vendor/perforce/bin/p4merge.dotslash
+
+ln -s -f p4merge.dotslash ./contrib/vendor/perforce/bin/p4merge
