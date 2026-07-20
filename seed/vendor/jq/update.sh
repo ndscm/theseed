137217466d84f80b626f64c4b885a30c19c088c4
Nagi <nagi@ndscm.com>
2026-07-20 10:47:00 -0700

    seed: vendor: jq: update dotslash with explicit format and replacement
    Change-uuid: d4a17820-1312-40a9-a80b-e53eb20c7c1e
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/jq/update.sh
+++ seed/vendor/jq/update.sh
@@ -3,11 +3,10 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="1.8.1"
+tag="${1:-"1.8.1"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/jq/jq.dotslash.json" \
-  --tag="${tag}" \
-  --no-format \
+  --replace "TAG=${tag}" \
   >./seed/vendor/jq/bin/jq
 chmod +x ./seed/vendor/jq/bin/jq
