bc9b0a802519303227c6103e1308b5e0ada6ccd7
Nagi <nagi@ndscm.com>
2026-07-20 10:43:45 -0700

    seed: vendor: bazel: update dotslash with explicit format and replacement
    Change-uuid: ebbc987f-7b1f-49ba-b81c-823ec1ac37fb
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/bazel/update.sh
+++ seed/vendor/bazel/update.sh
@@ -3,9 +3,10 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
+tag="${1:-"v1.29.0"}"
+
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/bazel/bazelisk.dotslash.json" \
-  --no-format \
-  --tag="v1.29.0" \
+  --replace "TAG=${tag}" \
   >./seed/vendor/bazel/bin/bazelisk
 chmod +x ./seed/vendor/bazel/bin/bazelisk
