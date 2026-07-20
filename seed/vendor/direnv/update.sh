26cca42ab65c07ccded05b3962d264f021226d58
Nagi <nagi@ndscm.com>
2026-07-20 13:40:56 -0700

    seed: vendor: direnv: update with explicit dotslash extension
    Change-uuid: 725a4ea5-0a71-416b-900a-7e08b9c9cdc9
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/direnv/update.sh
+++ seed/vendor/direnv/update.sh
@@ -8,5 +8,8 @@ tag="${1:-"v2.37.1"}"
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/direnv/direnv.dotslash.json" \
   --replace "TAG=${tag}" \
-  >./seed/vendor/direnv/bin/direnv
-chmod +x ./seed/vendor/direnv/bin/direnv
+  --outdir "$(pwd)/seed/vendor/direnv/bin"
+
+chmod +x ./seed/vendor/direnv/bin/direnv.dotslash
+
+ln -s -f direnv.dotslash ./seed/vendor/direnv/bin/direnv
