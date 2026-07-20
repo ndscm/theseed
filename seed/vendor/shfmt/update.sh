8c36fb1d0492a3b9c9dc406f3a0e0205e6977775
Nagi <nagi@ndscm.com>
2026-07-20 10:50:52 -0700

    seed: vendor: shfmt: update dotslash with explicit format and replacement
    Change-uuid: 8621d4ca-7b53-4581-a04b-da6807d485ef
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/shfmt/update.sh
+++ seed/vendor/shfmt/update.sh
@@ -3,11 +3,10 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="v3.13.1"
+tag="${1:-"v3.13.1"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/shfmt/shfmt.dotslash.json" \
-  --tag="${tag}" \
-  --no-format \
+  --replace "TAG=${tag}" \
   >./seed/vendor/shfmt/bin/shfmt
 chmod +x ./seed/vendor/shfmt/bin/shfmt
