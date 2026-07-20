a0d142f5f2924a9cd90cd16aa3355ce819d541ff
Nagi <nagi@ndscm.com>
2026-07-20 10:49:43 -0700

    seed: vendor: llvm: update dotslash with explicit format and replacement
    Change-uuid: e5563d73-77a8-4496-ad0a-f858d8ae78ef
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/llvm/update.sh
+++ seed/vendor/llvm/update.sh
@@ -3,22 +3,22 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="22.1.7"
+tag="${1:-"22.1.7"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/llvm/clang-format.dotslash.json" \
-  --tag="${tag}" \
+  --replace "TAG=${tag}" \
   >./seed/vendor/llvm/bin/clang-format
 chmod +x ./seed/vendor/llvm/bin/clang-format
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/llvm/clang.dotslash.json" \
-  --tag="${tag}" \
+  --replace "TAG=${tag}" \
   >./seed/vendor/llvm/bin/clang
 chmod +x ./seed/vendor/llvm/bin/clang
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/llvm/clangd.dotslash.json" \
-  --tag="${tag}" \
+  --replace "TAG=${tag}" \
   >./seed/vendor/llvm/bin/clangd
 chmod +x ./seed/vendor/llvm/bin/clangd
