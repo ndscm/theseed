0c3e286c849774c274be9b7fdf7e01fd5403f190
Nagi <nagi@ndscm.com>
2026-07-20 10:50:37 -0700

    seed: vendor: rust: update dotslash with explicit format and replacement
    Change-uuid: 7162de3f-f6c8-4988-85d2-e132083465cc
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/rust/update.sh
+++ seed/vendor/rust/update.sh
@@ -3,11 +3,10 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="1.96.0"
+tag="${1:-"1.96.0"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/rust/rustup-init.dotslash.json" \
-  --no-format \
-  --tag="${tag}" \
+  --replace "TAG=${tag}" \
   >./seed/vendor/rust/bin/rustup-init
 chmod +x ./seed/vendor/rust/bin/rustup-init
