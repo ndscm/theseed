009ae33cdcc4332c33b3a44c105fa007638e224f
Nagi <nagi@ndscm.com>
2026-07-20 10:43:34 -0700

    seed: vendor: age: update dotslash with explicit format and replacement
    Change-uuid: 6d3550f3-2874-4d4d-bb60-bcbe4eb648f7
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/age/update.sh
+++ seed/vendor/age/update.sh
@@ -3,22 +3,22 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="v1.3.1"
+tag="${1:-"v1.3.1"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/age/age.dotslash.json" \
-  --tag="${tag}" \
+  --replace "TAG=${tag}" \
   >./seed/vendor/age/bin/age
 chmod +x ./seed/vendor/age/bin/age
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/age/age-keygen.dotslash.json" \
-  --tag="${tag}" \
+  --replace "TAG=${tag}" \
   >./seed/vendor/age/bin/age-keygen
 chmod +x ./seed/vendor/age/bin/age-keygen
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/age/age-inspect.dotslash.json" \
-  --tag="${tag}" \
+  --replace "TAG=${tag}" \
   >./seed/vendor/age/bin/age-inspect
 chmod +x ./seed/vendor/age/bin/age-inspect
