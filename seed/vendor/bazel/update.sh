800513ad738cbd349081b256a40dd110ab6e25e4
Nagi <nagi@ndscm.com>
2026-07-20 13:36:24 -0700

    seed: vendor: bazel: update with explicit dotslash extension
    Change-uuid: b6ada1da-0ae2-439c-8182-2df220a4163c
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/bazel/update.sh
+++ seed/vendor/bazel/update.sh
@@ -8,5 +8,9 @@ tag="${1:-"v1.29.0"}"
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/bazel/bazelisk.dotslash.json" \
   --replace "TAG=${tag}" \
-  >./seed/vendor/bazel/bin/bazelisk
-chmod +x ./seed/vendor/bazel/bin/bazelisk
+  --outdir "$(pwd)/seed/vendor/bazel/bin"
+
+chmod +x ./seed/vendor/bazel/bin/bazelisk.dotslash
+
+ln -s -f bazelisk.dotslash ./seed/vendor/bazel/bin/bazel
+ln -s -f bazelisk.dotslash ./seed/vendor/bazel/bin/bazelisk
