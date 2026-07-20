d9d8592d37311da866bfc299dfd9ce4f02d363db
Nagi <nagi@ndscm.com>
2026-07-20 10:47:38 -0700

    seed: vendor: kubernetes: update dotslash with explicit format and replacement
    Change-uuid: 6ddb4ee4-8767-4f5b-9a34-4a92a587597e
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/kubernetes/update.sh
+++ seed/vendor/kubernetes/update.sh
@@ -3,20 +3,17 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="v1.36.2"
+kubectl="${1:-"v1.36.2"}"
+minikube="${2:-"v1.38.1"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/kubernetes/kubectl.dotslash.json" \
-  --no-format \
-  --tag="${tag}" \
+  --replace "TAG=${kubectl}" \
   >./seed/vendor/kubernetes/bin/kubectl
 chmod +x ./seed/vendor/kubernetes/bin/kubectl
 
-minikube="v1.38.1"
-
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/kubernetes/minikube.dotslash.json" \
-  --no-format \
-  --tag="${minikube}" \
+  --replace "TAG=${minikube}" \
   >./seed/vendor/kubernetes/bin/minikube
 chmod +x ./seed/vendor/kubernetes/bin/minikube
