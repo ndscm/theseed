fd79444d99fca69b5d2fb96a2ec51b772b90fb46
Nagi <nagi@ndscm.com>
2026-07-20 14:02:09 -0700

    seed: vendor: node: update with explicit dotslash extension
    Change-uuid: 52b6e992-c143-4ac3-afe9-9125f70ab461
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/node/update.sh
+++ seed/vendor/node/update.sh
@@ -6,25 +6,23 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 tag="${1:-"v24.14.1"}"
 
 bazel run //seed/devprod/dotslash/update -- \
+  --skeleton "$(pwd)/seed/vendor/node/corepack.dotslash.json" \
   --skeleton "$(pwd)/seed/vendor/node/node.dotslash.json" \
-  --replace "TAG=${tag}" \
-  >./seed/vendor/node/bin/node
-chmod +x ./seed/vendor/node/bin/node
-
-bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/node/npm.dotslash.json" \
-  --replace "TAG=${tag}" \
-  >./seed/vendor/node/bin/npm
-chmod +x ./seed/vendor/node/bin/npm
-
-bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/node/npx.dotslash.json" \
   --replace "TAG=${tag}" \
-  >./seed/vendor/node/bin/npx
-chmod +x ./seed/vendor/node/bin/npx
+  --outdir "$(pwd)/seed/vendor/node/bin"
 
-bazel run //seed/devprod/dotslash/update -- \
-  --skeleton "$(pwd)/seed/vendor/node/corepack.dotslash.json" \
-  --replace "TAG=${tag}" \
-  >./seed/vendor/node/bin/corepack
-chmod +x ./seed/vendor/node/bin/corepack
+chmod +x ./seed/vendor/node/bin/corepack.dotslash
+chmod +x ./seed/vendor/node/bin/node.dotslash
+chmod +x ./seed/vendor/node/bin/npm.dotslash
+chmod +x ./seed/vendor/node/bin/npx.dotslash
+
+ln -s -f corepack.dotslash ./seed/vendor/node/bin/corepack
+ln -s -f node.dotslash ./seed/vendor/node/bin/node
+ln -s -f npm.dotslash ./seed/vendor/node/bin/npm
+ln -s -f npx.dotslash ./seed/vendor/node/bin/npx
+ln -s -f pnpm.shim ./seed/vendor/node/bin/pnpm
+ln -s -f pnpx.shim ./seed/vendor/node/bin/pnpx
+ln -s -f yarn.shim ./seed/vendor/node/bin/yarn
+ln -s -f yarnpkg.shim ./seed/vendor/node/bin/yarnpkg
