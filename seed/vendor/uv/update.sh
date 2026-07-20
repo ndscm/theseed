87c6da6d20a6234bb23e42d6b00cb1e9b42dcda9
Nagi <nagi@ndscm.com>
2026-07-20 10:51:20 -0700

    seed: vendor: uv: update dotslash with explicit format and replacement
    Change-uuid: 29a573fd-cf49-421d-a1e0-46d8799df814
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/uv/update.sh
+++ seed/vendor/uv/update.sh
@@ -3,16 +3,16 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="0.11.21"
+tag="${1:-"0.11.21"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/uv/uv.dotslash.json" \
-  --tag="${tag}" \
+  --replace "TAG=${tag}" \
   >./seed/vendor/uv/bin/uv
 chmod +x ./seed/vendor/uv/bin/uv
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/uv/uvx.dotslash.json" \
-  --tag="${tag}" \
+  --replace "TAG=${tag}" \
   >./seed/vendor/uv/bin/uvx
 chmod +x ./seed/vendor/uv/bin/uvx
