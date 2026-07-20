090d80a3de35bc3deb4ffc448582acd4ff7a2be4
Nagi <nagi@ndscm.com>
2026-07-20 10:49:52 -0700

    seed: vendor: ndscm: update dotslash with explicit format and replacement
    Change-uuid: 2757df48-d4c6-4934-b69f-a5b103bdb96a
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/ndscm/update.sh
+++ seed/vendor/ndscm/update.sh
@@ -3,10 +3,10 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="v26.7.4"
+tag="${1:-"v26.7.4"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/ndscm/ndscm.dotslash.json" \
-  --tag="${tag}" \
+  --replace "TAG=${tag}" \
   >./seed/vendor/ndscm/bin/ndscm
 chmod +x ./seed/vendor/ndscm/bin/ndscm
