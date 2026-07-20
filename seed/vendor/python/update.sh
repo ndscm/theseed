9d2446d3c9c8cfed297537e05f97e6785549f933
Nagi <nagi@ndscm.com>
2026-07-20 08:57:15 -0700

    seed: vendor: python: use multiple replacement for python3 dotslash template
    Change-uuid: 05593d81-a120-4ea3-99c5-3bb13c4576e6
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/python/update.sh
+++ seed/vendor/python/update.sh
@@ -3,10 +3,12 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="3.11.15"
+tag="${1:-"20260623"}"
+version="${2:-"3.11.15"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/python/python3.dotslash.json" \
-  --tag="${tag}" \
+  --replace "TAG=${tag}" \
+  --replace "VERSION=${version}" \
   >./seed/vendor/python/bin/python3
 chmod +x ./seed/vendor/python/bin/python3
