d51022234d938a63c1044607c96ec05675a529c2
Nagi <nagi@ndscm.com>
2026-07-21 06:39:03 -0700

    seed: hooin: start development server with ndscm secret
    Change-uuid: 12b1865e-3147-441e-9d2e-954556a4548d
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/hooin/server/start.sh
+++ seed/newtype/hooin/server/start.sh
@@ -4,5 +4,6 @@ set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 
 bazel run //seed/newtype/hooin/server -- \
-  --static_team_file=$(pwd)/seed/newtype/hooin/container/team.json \
+  --openid_client_id "hooin-dev-${ND_USER_HANDLE}" \
+  --openid_client_secret_file "sops:$(ndscm secret --user get-path seed/newtype/hooin/server/OPENID_CLIENT_SECRET.sops)" \
   --verbose
