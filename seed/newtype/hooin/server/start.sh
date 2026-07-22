f2993e1baafa2e59f877f0232dbb5efd1dccb2f5
Nagi <nagi@ndscm.com>
2026-07-22 09:36:52 -0700

    seed: hooin: load secret with age flag
    Change-uuid: 9581a298-da40-4f53-9add-829a2688e747
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/hooin/server/start.sh
+++ seed/newtype/hooin/server/start.sh
@@ -4,6 +4,7 @@ set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 
 bazel run //seed/newtype/hooin/server -- \
+  --age_key_file "${AGE_KEY_FILE}" \
   --openid_client_id "hooin-dev-${ND_USER_HANDLE}" \
-  --openid_client_secret_file "sops:$(ndscm secret --user get-path seed/newtype/hooin/server/OPENID_CLIENT_SECRET.sops)" \
+  --openid_client_secret_file "age:$(ndscm secret --user get-path seed/newtype/hooin/server/OPENID_CLIENT_SECRET.age)" \
   --verbose
