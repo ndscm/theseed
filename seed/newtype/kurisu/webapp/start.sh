5dce02698b8e9e7af417991757ed1aca7377e80b
Nagi <nagi@ndscm.com>
2026-07-26 10:36:25 -0700

    seed: kurisu: try session age key for development server
    Change-uuid: c16dd940-699f-4690-8195-ce06ac09525a
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/kurisu/webapp/start.sh
+++ seed/newtype/kurisu/webapp/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
@@ -17,16 +17,20 @@ export LOGIN_SERVICE_SERVER="http://127.0.0.1:5874"
 export OPENID_CLIENT_ID="kurisu-webapp-dev"
 if [[ -f "$(ndscm secret --user get-path seed/newtype/kurisu/webapp/OPENID_CLIENT_SECRET.age)" ]]; then
   export OPENID_CLIENT_ID="kurisu-webapp-dev-${ND_USER_HANDLE}"
-  secret_file="$(mktemp)"
-  exec {secret_fd}<>"${secret_file}"
-  rm -f "${secret_file}"
   set +x
-  age -d \
-    -i "$(ndscm secret --user get-path key.age)" \
-    "$(ndscm secret --user get-path seed/newtype/kurisu/webapp/OPENID_CLIENT_SECRET.age)" \
-    >&"$secret_fd"
+  age_key_file="${AGE_KEY_FILE:-""}"
+  if [[ -z "${age_key_file}" ]]; then
+    age_key_file="$(ndscm secret --user get-path key.age)"
+  fi
+  openid_client_secret="$(
+    age -d \
+      -i "${age_key_file}" \
+      "$(ndscm secret --user get-path seed/newtype/kurisu/webapp/OPENID_CLIENT_SECRET.age)"
+  )"
+  exec {openid_client_secret_fd}< <(printf "%s" "${openid_client_secret}")
+  unset openid_client_secret
   set -x
-  export OPENID_CLIENT_SECRET_FILE="/proc/$$/fd/$secret_fd"
+  export OPENID_CLIENT_SECRET_FILE="/proc/$$/fd/${openid_client_secret_fd}"
 fi
 
 npx react-router dev --host
