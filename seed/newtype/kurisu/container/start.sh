90230d56eef2832979f4f0438eb56b6d4c92d262
Nagi <nagi@ndscm.com>
2026-07-28 07:09:11 -0700

    seed: kurisu: start development container with bazel built image
    Change-uuid: 92c94445-abc5-4fa2-9f00-ad4d3899c5e9
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/kurisu/container/start.sh
+++ seed/newtype/kurisu/container/start.sh
@@ -5,9 +5,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 container_engine=${CONTAINER_ENGINE:-"podman"}
 
-export CONTAINER_ENGINE="${container_engine}"
-./build.sh
+bazel run --@rules_img//img/settings:load_daemon="${container_engine}" //seed/newtype/kurisu/container:load
 
 "${container_engine}" run --name "kurisu" --rm --interactive --tty \
   --network "host" \
+  --env SEED_OPENID_DISCOVERY_URL \
   ghcr.io/ndscm/seed-newtype-kurisu-container:latest
