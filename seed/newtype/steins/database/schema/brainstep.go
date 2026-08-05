81d475ebfa5509d68165a12340222f3282081ac8
Nagi <nagi@ndscm.com>
2026-08-04 19:48:33 -0700

    seed: steins: database: complete brain step fields
    Change-uuid: 1d71f06c-8f3a-4d5b-9c51-cc4ba2f8c249
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/steins/database/schema/brainstep.go
+++ seed/newtype/steins/database/schema/brainstep.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"entgo.io/ent"
-	"entgo.io/ent/dialect/entsql"
 	"entgo.io/ent/schema/field"
 	"github.com/google/uuid"
 )
@@ -17,9 +16,14 @@ type BrainStep struct {
 // Fields of the BrainStep.
 func (BrainStep) Fields() []ent.Field {
 	return []ent.Field{
-		field.UUID("id", uuid.UUID{}).Unique().Immutable().Default(uuid.New).Annotations(entsql.Default("gen_random_uuid()")),
+		field.UUID("id", uuid.UUID{}).StorageKey("uuid").Unique().Immutable().Default(uuid.New),
 		field.Time("create_time").Immutable().Default(time.Now),
-		field.Time("update_time").Default(time.Now).UpdateDefault(time.Now),
+		field.String("person_id").Immutable(),
+		field.Time("emit_time").Immutable(),
+		field.String("type").Immutable(),
+		field.String("topic").Immutable(),
+		field.String("thread_uuid").Immutable(),
+		field.JSON("data", map[string]any{}).Optional(),
 	}
 }
 
