7adf65c00392f15a5212e4a1caa1082dc2b81ca7
Nagi <nagi@ndscm.com>
2026-08-04 20:28:06 -0700

    seed: hooin: start server with local db
    Change-uuid: 32a5ec28-8ad2-4f03-a634-7893542f0e47
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/newtype/hooin/server/start.sh
+++ seed/newtype/hooin/server/start.sh
@@ -4,20 +4,33 @@ set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 
 set +x
+
 age_key_file="${AGE_KEY_FILE:-""}"
 if [[ -z "${age_key_file}" ]]; then
   age_key_file="$(ndscm secret --user get-path key.age)"
 fi
+
 openid_client_secret="$(
-  age -d \
-    -i "${age_key_file}" \
+  age -d -i "${age_key_file}" \
     "$(ndscm secret --user get-path seed/newtype/hooin/server/OPENID_CLIENT_SECRET.age)"
 )"
 exec {openid_client_secret_fd}< <(printf "%s" "${openid_client_secret}")
 unset openid_client_secret
+
+steins_database_secret="$(
+  age -d -i "${age_key_file}" \
+    "$(ndscm secret --user get-path seed/newtype/steins/database/STEINS_DATABASE_SECRET.local.age)"
+)"
+exec {steins_database_secret_fd}< <(printf "%s" "${steins_database_secret}")
+unset steins_database_secret
+
 set -x
 
 bazel run //seed/newtype/hooin/server -- \
   --openid_client_id "hooin-dev-${ND_USER_HANDLE}" \
   --openid_client_secret_file "/proc/$$/fd/${openid_client_secret_fd}" \
+  --steins_database "postgres://127.0.0.1:5432/steins_local" \
+  --steins_database_debug \
+  --steins_database_login "steins_local" \
+  --steins_database_secret_file "/proc/$$/fd/${steins_database_secret_fd}" \
   --verbose
