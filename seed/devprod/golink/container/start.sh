c9da996916cd8a315756533ba8c73b9bc09cf40a
Nagi <nagi@ndscm.com>
2026-08-06 01:48:39 -0700

    seed: golink: build container with bazel
    Change-uuid: 1b1e4472-bdaa-42d6-a8b1-ae0345ba3c2e
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/golink/container/start.sh
+++ seed/devprod/golink/container/start.sh
@@ -5,8 +5,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 container_engine=${CONTAINER_ENGINE:-"podman"}
 
-export CONTAINER_ENGINE="${container_engine}"
-./build.sh
+bazel run \
+  --@rules_img//img/settings:load_daemon="${container_engine}" \
+  //seed/devprod/golink/container:load
 
 "${container_engine}" run --name "golink" --rm --interactive --tty \
   --network "host" \
