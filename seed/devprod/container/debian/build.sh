1361f58461f050dc06ad1b512aa98820f3b299db
Nagi <nagi@ndscm.com>
2026-08-06 08:58:33 -0700

    seed: container: debian: build debian 13 freedom with podman in bazel
    Change-uuid: dd0a7766-ff4a-454f-9454-3925314fe9aa
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/container/debian/build.sh
+++ seed/devprod/container/debian/build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -eux
 set -o pipefail
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 
 container_engine=${CONTAINER_ENGINE:-"podman"}
 
-"${container_engine}" build -f ./Containerfile -t ghcr.io/ndscm/debian:latest .
+bazel build //seed/devprod/container/debian:debian_13_freedom
+
+"${container_engine}" load \
+  -i "bazel-bin/seed/devprod/container/debian/debian_13_freedom.tar"
+"${container_engine}" tag \
+  ghcr.io/ndscm/debian:13-freedom \
+  ghcr.io/ndscm/debian:latest
