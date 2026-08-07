626c5ce7ad25a519c58de97f04ec833c27a4c4b0
Nagi <nagi@ndscm.com>
2026-08-07 01:23:59 -0700

    seed: container: ubuntu: build freedom container
    Change-uuid: 35d6317f-0aaa-48ae-874f-1b50f6957b35
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/container/ubuntu/build.sh
+++ seed/devprod/container/ubuntu/build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -eux
 set -o pipefail
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 
 container_engine=${CONTAINER_ENGINE:-"podman"}
 
-"${container_engine}" build -f ./Containerfile -t ghcr.io/ndscm/ubuntu:latest .
+bazel build //seed/devprod/container/ubuntu:ubuntu_2604_freedom
+
+"${container_engine}" load \
+  -i "bazel-bin/seed/devprod/container/ubuntu/ubuntu_2604_freedom.tar"
+"${container_engine}" tag \
+  ghcr.io/ndscm/ubuntu:2604-freedom \
+  ghcr.io/ndscm/ubuntu:latest
