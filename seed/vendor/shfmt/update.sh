ec6adda188eceb0c7f77553589e3904d01dc6f2a
Nagi <nagi@ndscm.com>
2026-08-30 05:21:22 -0700

    seed: vendor: shfmt: bump cli versions
    Change-uuid: f71aed95-0602-4e94-bfb0-f9e780a0a2a1
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/shfmt/update.sh
+++ seed/vendor/shfmt/update.sh
@@ -3,7 +3,7 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="${1:-"v3.13.1"}"
+tag="${1:-"v3.14.0"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/shfmt/shfmt.dotslash.json" \
