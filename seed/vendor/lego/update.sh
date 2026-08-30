7c2c0ef4e9fd03020d654d631b2f3f63a1d2d172
Nagi <nagi@ndscm.com>
2026-08-30 05:12:46 -0700

    seed: vendor: lego: bump cli versions
    Change-uuid: 844ab596-d1d8-4f99-852b-4aef5902c0d1
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/lego/update.sh
+++ seed/vendor/lego/update.sh
@@ -3,7 +3,7 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="${1:-"v5.1.0"}"
+tag="${1:-"v5.4.0"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/lego/lego.dotslash.json" \
