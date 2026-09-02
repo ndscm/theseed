9d492a17aec48b94ca0ea5710cc2725a6bab3a0d
Nagi <nagi@ndscm.com>
2026-09-02 02:19:34 -0700

    seed: vendor: rust: bump rustup version
    Change-uuid: 2677543b-2222-4d70-b7a1-ed5f2d887451
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/rust/update.sh
+++ seed/vendor/rust/update.sh
@@ -3,11 +3,11 @@ set -eux
 set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
-tag="${1:-"1.96.0"}"
+rustup="${1:-"1.29.1"}"
 
 bazel run //seed/devprod/dotslash/update -- \
   --skeleton "$(pwd)/seed/vendor/rust/rustup-init.dotslash.json" \
-  --replace "TAG=${tag}" \
+  --replace "TAG=${rustup}" \
   --outdir "$(pwd)/seed/vendor/rust/bin"
 
 chmod +x ./seed/vendor/rust/bin/rustup-init.dotslash
