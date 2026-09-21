8543925b5f97c1a09b85d0e86bd3285a96539536
Nagi <nagi@ndscm.com>
2026-09-21 12:35:42 -0700

    seed: vendor: node: remove corepack
    corepack is no longer distributed with nodejs
    
    Change-uuid: 2024a723-f171-44af-8545-9ea0ccb52735
    Side-effect-of-change-uuid: eb55bc4f-76d6-47dc-880e-4627df81193b
    Break: nodejs no longer ships corepack
    Migrate: use prebuilt pnpm
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/vendor/node/update.sh
+++ seed/vendor/node/update.sh
@@ -6,23 +6,16 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 tag="${1:-"v26.7.0"}"
 
 bazel run //seed/devprod/dotslash/update -- \
-  --skeleton "$(pwd)/seed/vendor/node/corepack.dotslash.json" \
   --skeleton "$(pwd)/seed/vendor/node/node.dotslash.json" \
   --skeleton "$(pwd)/seed/vendor/node/npm.dotslash.json" \
   --skeleton "$(pwd)/seed/vendor/node/npx.dotslash.json" \
   --replace "TAG=${tag}" \
   --outdir "$(pwd)/seed/vendor/node/bin"
 
-chmod +x ./seed/vendor/node/bin/corepack.dotslash
 chmod +x ./seed/vendor/node/bin/node.dotslash
 chmod +x ./seed/vendor/node/bin/npm.dotslash
 chmod +x ./seed/vendor/node/bin/npx.dotslash
 
-ln -s -f corepack.dotslash ./seed/vendor/node/bin/corepack
 ln -s -f node.dotslash ./seed/vendor/node/bin/node
 ln -s -f npm.dotslash ./seed/vendor/node/bin/npm
 ln -s -f npx.dotslash ./seed/vendor/node/bin/npx
-ln -s -f pnpm.shim ./seed/vendor/node/bin/pnpm
-ln -s -f pnpx.shim ./seed/vendor/node/bin/pnpx
-ln -s -f yarn.shim ./seed/vendor/node/bin/yarn
-ln -s -f yarnpkg.shim ./seed/vendor/node/bin/yarnpkg
