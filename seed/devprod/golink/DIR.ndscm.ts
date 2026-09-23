408a57217eeca025d8d0981cfd5bb4356d175787
Nagi <nagi@ndscm.com>
2026-09-23 09:24:55 -0700

    seed: golink: add granularity configs
    Change-uuid: 3024c7f6-bc32-486d-adc8-ab21714354e7
    Signed-off-by: Nagi <nagi@ndscm.com>

---
+++ seed/devprod/golink/DIR.ndscm.ts
@@ -0,0 +1,8 @@
+import { type DirConfig } from "@//seed/devprod/ndscm/config/DIR"
+
+export default {
+  granularity: {
+    style: "linux",
+    prefix: "seed: golink: ",
+  },
+} satisfies DirConfig
