4cf13b274db54495240d6a7ae790f10d7bb3941a
Nagi <nagi@ndscm.com>
2026-09-23 11:04:57 -0700

    seed: buildinfo: ts: add granularity configs
    Change-uuid: 02f44783-57ac-4caa-9e5a-64cee50fd71f
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/infra/buildinfo/ts/DIR.ndscm.ts
+++ seed/infra/buildinfo/ts/DIR.ndscm.ts
@@ -1,6 +1,10 @@
 import { type DirConfig } from "@//seed/devprod/ndscm/config/DIR"
 
 export default {
+  granularity: {
+    style: "linux",
+    prefix: "seed: buildinfo: ts: ",
+  },
   bootstrap: {
     dist: {
       target: "dist",
