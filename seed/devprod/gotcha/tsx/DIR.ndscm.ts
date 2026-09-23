c92e58e2f0f840fcb4a19e7acd5a12b8a1a569b1
Nagi <nagi@ndscm.com>
2026-09-23 10:52:33 -0700

    seed: gotcha: tsx: add granularity configs
    Change-uuid: 07a46c32-94d5-4465-8273-b6d7dde7bc1d
    Signed-off-by: Nagi <nagi@ndscm.com>

--- seed/devprod/gotcha/tsx/DIR.ndscm.ts
+++ seed/devprod/gotcha/tsx/DIR.ndscm.ts
@@ -1,6 +1,10 @@
 import { type DirConfig } from "@//seed/devprod/ndscm/config/DIR"
 
 export default {
+  granularity: {
+    style: "linux",
+    prefix: "seed: gotcha: tsx: ",
+  },
   bootstrap: {
     dist: {
       target: "dist",
